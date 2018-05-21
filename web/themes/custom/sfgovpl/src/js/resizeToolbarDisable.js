2b491f4c96aeca43be2a2624bb054bdb9d7c886b
Israel M <isramv@gmail.com>
2018-05-21 18:07:25 -0500

    refactoring includes

---
+++ web/themes/custom/sfgovpl/src/js/resizeToolbarDisable.js
@@ -0,0 +1,7 @@
+Drupal.behaviors.dieToolbardie = {
+  attach: function (context) {
+    window.matchMedia('(min-width: 975px)').addListener( function(event) {
+      event.matches ? $('#toolbar-item-administration', context).click() : $('.toolbar-item.is-active', context).click();
+    });
+  }
+};
