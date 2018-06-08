a464a44c9bc49e250d555a7ed1f73fd4b2fd8411
Arlina Espinoza Rhoton <arlina.espinoza@gmail.com>
2018-06-08 13:51:05 -0700

    SG-114 media/groups (#54)
    * [SG-114] Media entities can be linked to departments.
    
    * [SG-114] Media entities - department groups permissions.
    
    * [SG-114] Media entities - department groups unit test.

---
+++ web/modules/custom/sfgov_departments/src/Plugin/GroupContentEnabler/GroupMedia.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Drupal\sfgov_departments\Plugin\GroupContentEnabler;
+
+use Drupal\group\Entity\GroupInterface;
+use Drupal\group\Plugin\GroupContentEnablerBase;
+use Drupal\media\Entity\MediaType;
+use Drupal\Core\Url;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a content enabler for media.
+ *
+ * @GroupContentEnabler(
+ *   id = "group_media",
+ *   label = @Translation("Group media"),
+ *   description = @Translation("Adds media to groups both publicly and privately."),
+ *   entity_type_id = "media",
+ *   entity_access = TRUE,
+ *   reference_label = @Translation("Title"),
+ *   reference_description = @Translation("The title of the media to add to the group"),
+ *   deriver = "Drupal\sfgov_departments\Plugin\GroupContentEnabler\GroupMediaDeriver"
+ * )
+ */
+class GroupMedia extends GroupContentEnablerBase {
+
+  /**
+   * Retrieves the media type this plugin supports.
+   *
+   * @return \Drupal\media\MediaTypeInterface
+   *   The media type this plugin supports.
+   */
+  protected function getMediaType() {
+    return MediaType::load($this->getEntityBundle());
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getGroupOperations(GroupInterface $group) {
+    $account = \Drupal::currentUser();
+    $plugin_id = $this->getPluginId();
+    $type = $this->getEntityBundle();
+    $operations = [];
+
+    if ($group->hasPermission("create $plugin_id entity", $account)) {
+      $route_params = ['group' => $group->id(), 'plugin_id' => $plugin_id];
+      $operations["gmedia-create-$type"] = [
+        'title' => $this->t('Create @type', ['@type' => $this->getMediaType()->label()]),
+        'url' => new Url('entity.group_content.create_form', $route_params),
+        'weight' => 30,
+      ];
+    }
+
+    return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getTargetEntityPermissions() {
+    $permissions = parent::getTargetEntityPermissions();
+    $plugin_id = $this->getPluginId();
+
+    // Add a 'view unpublished' permission by re-using most of the 'view' one.
+    $original = $permissions["view $plugin_id entity"];
+    $permissions["view unpublished $plugin_id entity"] = [
+      'title' => str_replace('View ', 'View unpublished ', $original['title']),
+    ] + $original;
+
+    return $permissions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    $config = parent::defaultConfiguration();
+    $config['entity_cardinality'] = 1;
+    return $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+    // Disable the entity cardinality field as the functionality of this module
+    // relies on a cardinality of 1. We don't just hide it, though, to keep a UI
+    // that's consistent with other content enabler plugins.
+    $info = $this->t("This field has been disabled by the plugin to guarantee the functionality that's expected of it.");
+    $form['entity_cardinality']['#disabled'] = TRUE;
+    $form['entity_cardinality']['#description'] .= '<br /><em>' . $info . '</em>';
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function calculateDependencies() {
+    $dependencies = parent::calculateDependencies();
+    $dependencies['config'][] = 'media.type.' . $this->getEntityBundle();
+    return $dependencies;
+  }
+
+}
--- web/modules/custom/sfgov_departments/src/SFgovDepartment.php
+++ web/modules/custom/sfgov_departments/src/SFgovDepartment.php
@@ -4,6 +4,7 @@ namespace Drupal\sfgov_departments;
 
 use Drupal\group\Entity\GroupInterface;
 use Drupal\group\Entity\GroupType;
+use Drupal\media\MediaInterface;
 use Drupal\node\NodeInterface;
 
 /**
@@ -67,7 +68,7 @@ class SFgovDepartment {
 
     /** @var \Drupal\group\Entity\GroupContentInterface $group_content */
     $group_content = $entity_type_manager->getStorage('group_content')->load(reset($ids));
-    return $group_content->getEntity();
+    return $group_content ? $group_content->getEntity() : NULL;
   }
 
   /**
@@ -145,9 +146,12 @@ class SFgovDepartment {
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
   protected static function addNodeToGroup(NodeInterface $node, GroupInterface $group) {
+    /** @var \Drupal\group\Plugin\GroupContentEnablerInterface $plugin */
+    $plugin = $group->getGroupType()->getContentPlugin('group_node:department');
     $entity_type_manager = \Drupal::entityTypeManager();
 
     $query = \Drupal::entityQuery('group_content')
+      ->condition('type', $plugin->getContentTypeConfigId())
       ->condition('gid', $group->id())
       ->condition('entity_id', $node->id())
       ->range(0, 1);
@@ -222,6 +226,58 @@ class SFgovDepartment {
   }
 
   /**
+   * Updates the groups an entity belongs to.
+   *
+   * @param \Drupal\media\MediaInterface $entity Entity that will get its groups updated.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public static function updateGroupMediaContent(MediaInterface $entity) {
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $group_content_storage = $entity_type_manager->getStorage('group_content');
+
+    $plugin_types = [];
+    $group_type = GroupType::load('department');
+    foreach ($group_type->getInstalledContentPlugins() as $plugin) {
+      if ($plugin->getPluginDefinition()['entity_type_id'] == 'media') {
+        $plugin_types[] = $plugin->getContentTypeConfigId();
+      }
+    }
+
+    // Get previous groups.
+    $previous_departments_ids = [];
+    $query = \Drupal::entityQuery('group_content')
+      ->condition('type', $plugin_types, 'IN')
+      ->condition('entity_id', $entity->id())
+      ->range(0, 1);
+    $content_in_groups = $group_content_storage->loadMultiple($query->execute());
+    foreach ($content_in_groups as $group_content) {
+      $referenced = $group_content->gid->referencedEntities();
+      $group = reset($referenced);
+      if ($department_node = self::getDepartmentNode($group)) {
+        $previous_departments_ids[$department_node->id()] = $group_content;
+      }
+    }
+
+    // Add to new departments.
+    $departments = $entity->get('field_department')->referencedEntities();
+    foreach ($departments as $department_node) {
+      if (in_array($department_node->id(), array_keys($previous_departments_ids))) {
+        unset($previous_departments_ids[$department_node->id()]);
+      }
+      else {
+        SFgovDepartment::addMediaToGroupByDepartmentNode($entity, $department_node);
+      }
+    }
+
+    // Delete from previous/removed departments.
+    if ($previous_departments_ids) {
+      $group_content_storage->delete($previous_departments_ids);
+    }
+  }
+
+  /**
    * Add a node to a group given the department node.
    *
    * @param \Drupal\node\NodeInterface $node Node to add to the group.
@@ -255,6 +311,46 @@ class SFgovDepartment {
         'gid' => $group->id(),
         'entity_id' => $node->id(),
         'label' => $node->getTitle(),
+      ]);
+      $group_content->save();
+    }
+  }
+
+  /**
+   * Add media to a group given the department node.
+   *
+   * @param \Drupal\media\MediaInterface $entity Entity to add to the group.
+   * @param \Drupal\node\NodeInterface $department Deparment node.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public static function addMediaToGroupByDepartmentNode(MediaInterface $entity, NodeInterface $department) {
+    $entity_type_manager = \Drupal::entityTypeManager();
+    $sf_gov_department = new self($department);
+    $group = $sf_gov_department->getDepartmentGroup();
+    $entity_type = $entity->getEntityType()->id();
+
+    /** @var \Drupal\group\Plugin\GroupContentEnablerInterface $plugin */
+    $plugin = $group->getGroupType()->getContentPlugin('group_' . $entity_type . ':' . $entity->bundle());
+
+    $query = \Drupal::entityQuery('group_content')
+      ->condition('type', $plugin->getContentTypeConfigId())
+      ->condition('gid', $group->id())
+      ->condition('entity_id', $entity->id())
+      ->range(0, 1);
+    $ids = $query->execute();
+
+    if (empty($ids)) {
+
+      /** @var \Drupal\group\Entity\Storage\GroupContentStorageInterface $group_content_storage */
+      $group_content_storage = $entity_type_manager->getStorage('group_content');
+
+      $group_content = $group_content_storage->create([
+        'type' => $plugin->getContentTypeConfigId(),
+        'gid' => $group->id(),
+        'entity_id' => $entity->id(),
+        'label' => $entity->label(),
       ]);
       $group_content->save();
     }
