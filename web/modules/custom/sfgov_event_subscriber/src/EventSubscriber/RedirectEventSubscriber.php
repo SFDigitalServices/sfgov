bb0e7e7914bc68bfb200259c42b751636cb6e972
Henry Jiang <henryjiang@TIS1SVNLTJIANH01.local>
2018-07-11 17:10:22 -0700

    SG-229

---
+++ web/modules/custom/sfgov_event_subscriber/src/EventSubscriber/RedirectEventSubscriber.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\sfgov_event_subscriber\EventSubscriber\RedirectEventSubscriber.
+ */
+
+namespace Drupal\sfgov_event_subscriber\EventSubscriber;
+
+use Drupal\Core\Url;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+//use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+//use Drupal\node\Entity\Node;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+
+/**
+ * Event Subscriber RedirectEventSubscriber.
+ */
+class RedirectEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+   // Drupal logger not working,  need to as CH3
+    
+    $events[KernelEvents::REQUEST][] = ['redirectBasedOnField'];
+    return $events;
+  }
+
+public function redirectBasedOnField(GetResponseEvent $event) {
+    // only redirect if in view mode. This relies on proper URL path setting for all nodes.  
+    $uri_array = explode('/',  $event->getRequest()->getRequestUri() ); 
+    if($uri_array[1] == 'node' && ( $uri_array[2] && is_numeric($uri_array[2]) ) && $uri_array[3] != '' ){
+        // do nothing
+        //\Drupal::logger('sfgov_event_subscriber')->notice('edit mode');
+    }
+    else {
+        $node = $event->getRequest()->attributes->get('node'); 
+      
+        if($node && $node->hasField('field_direct_external_url') ){
+          $field_external_url = $node->get('field_direct_external_url');
+          $external_url = $field_external_url->getValue()[0]['uri'];
+
+          if($external_url != '' ){  
+              // This is where you set the destination.
+              //$redirect_url = Url::fromUri($external_url->getValue());
+              $response = new TrustedRedirectResponse($external_url);
+              $event->setResponse($response);
+          }
+        }
+      }
+  }
+
+}
+
