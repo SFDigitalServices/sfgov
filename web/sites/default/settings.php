14afb7ee072f75ec250b6328b53e71b9f3e68668
Anthony Kong <anthony.kong@sfgov.org>
2018-08-17 09:21:33 -0700

    reduce nesting, remove unncessary conditional

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -54,27 +54,23 @@ include __DIR__ . "/settings.pantheon.php";
  */
 
 // force https on sf.gov
-if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
-  if($_ENV['PANTHEON_ENVIRONMENT'] != 'lando') { // lando is local env
-    // Redirect to HTTPS on every Pantheon environment.
-    $primary_domain = $_SERVER['HTTP_HOST'];
-    if ($_SERVER['HTTP_HOST'] != $primary_domain
-      || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
-      || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
-  
-      # Name transaction "redirect" in New Relic for improved reporting (optional)
-      if (extension_loaded('newrelic')) {
-          newrelic_name_transaction("redirect");
-      }
-  
-      header('HTTP/1.0 301 Moved Permanently');
-      header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
-      exit();
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && $_ENV['PANTHEON_ENVIRONMENT'] != 'lando' && php_sapi_name() != 'cli') {
+  // Redirect to HTTPS on every Pantheon environment.
+  $primary_domain = $_SERVER['HTTP_HOST'];
+  if (!isset($_SERVER['HTTP_USER_AGENT_HTTPS']) || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+
+    # Name transaction "redirect" in New Relic for improved reporting (optional)
+    if (extension_loaded('newrelic')) {
+        newrelic_name_transaction("redirect");
     }
-    // Drupal 8 Trusted Host Settings
-    if (is_array($settings)) {
-      $settings['trusted_host_patterns'] = array('^'. preg_quote($primary_domain) .'$');
-    }
+
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+  // Drupal 8 Trusted Host Settings
+  if (is_array($settings)) {
+    $settings['trusted_host_patterns'] = array('^'. preg_quote($primary_domain) .'$');
   }
 }
 
