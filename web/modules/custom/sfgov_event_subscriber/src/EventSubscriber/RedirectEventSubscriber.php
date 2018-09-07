6f04050dca613c8d5fcf883cdf98a8feb86f23cd
Anthony Kong <anthony.kong@sfgov.org>
2018-09-07 14:50:51 -0700

    remove unnecessary conditional

--- web/modules/custom/sfgov_event_subscriber/src/EventSubscriber/RedirectEventSubscriber.php
+++ web/modules/custom/sfgov_event_subscriber/src/EventSubscriber/RedirectEventSubscriber.php
@@ -18,42 +18,34 @@ use Drupal\Core\Session\AccountInterface;
  */
 class RedirectEventSubscriber implements EventSubscriberInterface {
 
-    /**
+  /**
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-   // Drupal logger not working,  need to as CH3
-    
+    // Drupal logger not working,  need to as CH3
+
     $events[KernelEvents::REQUEST][] = ['redirectBasedOnField'];
     return $events;
   }
 
   public function redirectBasedOnField(GetResponseEvent $event) {
-      // don't redirect logged in users.
-      $account = \Drupal::currentUser();
-      if ( !empty($account->id()) ) {
-          return;
-      }
-    // only redirect if in view mode. This relies on proper URL path setting for all nodes.  
-    $uri_array = explode('/',  $event->getRequest()->getRequestUri() ); 
-    if($uri_array[1] == 'node' && ( $uri_array[2] && is_numeric($uri_array[2]) ) && $uri_array[3] != '' ){
-        // do nothing
-        //\Drupal::logger('sfgov_event_subscriber')->notice('edit mode');
+    // don't redirect logged in users.
+    $account = \Drupal::currentUser();
+    if ( !empty($account->id()) ) {
+      return;
     }
-    else {
-        $node = $event->getRequest()->attributes->get('node'); 
-      
-        if($node && $node->hasField('field_direct_external_url') ){
-          $field_external_url = $node->get('field_direct_external_url')->getValue();
 
-          if( !empty($field_external_url[0]) && $field_external_url[0]['uri'] != ''){
-              // This is where you set the destination.
-              $response = new TrustedRedirectResponse($field_external_url[0]['uri']);
-              $event->setResponse($response);
-          }
-        }
+    $node = $event->getRequest()->attributes->get('node'); 
+
+    if($node && $node->hasField('field_direct_external_url') ){
+      $field_external_url = $node->get('field_direct_external_url')->getValue();
+
+      if( !empty($field_external_url[0]) && $field_external_url[0]['uri'] != ''){
+        // This is where you set the destination.
+        $response = new TrustedRedirectResponse($field_external_url[0]['uri']);
+        $event->setResponse($response);
       }
+    }
   }
-
 }
 
