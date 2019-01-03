a06515774767280312989745abb856307a1b060d
Anthony Kong <anthony.kong@sfgov.org>
2019-01-03 15:39:07 -0800

    collect top searches, add to container, and show hide

--- web/themes/custom/sfgovpl/src/js/search-top-suggestions.js
+++ web/themes/custom/sfgovpl/src/js/search-top-suggestions.js
@@ -1,1 +1,20 @@
-console.log('search top suggestions js');
+(function($) {
+  var topSearchSuggs = $('.sfgov-top-search-suggestion');
+  if(topSearchSuggs.length > 0) {
+    var containerId = 'sfgov-top-search-suggestions-container';
+    var containerSelector = '#' + containerId;
+    $('#edit-keyword').attr('autocomplete', 'off');
+    $(topSearchSuggs[0]).before('<div id="' + containerId + '"><h4>Top Searches:</h4></div>');
+    $(topSearchSuggs).each(function() {
+      $(containerSelector).append($(this));
+    });
+    $(containerSelector).hide();
+    $('#edit-keyword').focus(function() {
+      $(containerSelector).show();
+    });
+    $('#edit-keyword').blur(function() {
+      $(containerSelector).hide();
+    })
+  }
+
+})(jQuery);
