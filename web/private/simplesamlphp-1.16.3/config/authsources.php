26cd7bdfe87ec93fbcd70dac048cd76c5294ffa5
Josh Chou <626661+josh-chou@users.noreply.github.com>
2019-01-24 14:06:01 -0800

    don't send NameIDPolicy in authentication request

--- web/private/simplesamlphp-1.16.3/config/authsources.php
+++ web/private/simplesamlphp-1.16.3/config/authsources.php
@@ -17,6 +17,7 @@ $config = array(
         'saml:SP',
         'privatekey' => 'saml.pem',
         'certificate' => 'saml.crt',
+        'NameIDPolicy' => false,
 
         // The entity ID of this SP.
         // Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
