f5bcd063239da81babcc031b02d8d0bd4a0ab851
Anthony Kong <anthony.kong@sfgov.org>
2019-06-17 15:18:33 -0700

    gulp 4 + gulp css task

---
+++ web/themes/custom/sfgovpl/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  css: {
+    source: './src/sass/**/*.scss',
+    dest: './dist/css',
+    output: 'drupal.css'
+  },
+  images: {
+    source: './src/img/**',
+    dest: './dist/img',
+  }
+};
