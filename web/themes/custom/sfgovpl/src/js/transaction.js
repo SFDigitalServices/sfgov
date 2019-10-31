e08df099091a3e1a6a2766cfd36b386b4fd634e7
Anthony Kong <anthony.kong@sfgov.org>
2019-10-31 09:37:09 -0700

    sg-778 - inject the appropriate 'part of ' step-by-step based on url params

--- web/themes/custom/sfgovpl/src/js/transaction.js
+++ web/themes/custom/sfgovpl/src/js/transaction.js
@@ -1,7 +1,19 @@
 (function($) {
   $('.page-node-type-transaction').each(function() {
-    if(SFGOV.util.getParam('from') === 'sbs') {
-      $('.hero-banner-label').show();
+    if(SFGOV.util.getParam('from') === 'sbs' && SFGOV.util.getParam('txid') && SFGOV.util.getParam('sbsid')) {
+      var txid = SFGOV.util.getParam('txid');
+      var sbsid = SFGOV.util.getParam('sbsid');
+      // this path is a rest export defined in the view 'transaction_step_by_step'
+      $.ajax('/related-step/' + txid + '/' + sbsid, { 
+        success: function(data) {
+          var html = '';
+          if(data && data.length > 0) {
+            html += '<span>Part of </span><a href="' + data[0].url + '">Step by step: ' + data[0].title + '</a>';
+          }
+          $('.hero-banner--container').prepend('<div class="hero-banner-label">' + html + '</div>');
+        }
+      });
+
     }
   });
 }(jQuery));
