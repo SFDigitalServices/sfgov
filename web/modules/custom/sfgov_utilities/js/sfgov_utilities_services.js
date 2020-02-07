273e3ee2c150bbfe130b857395f9d7025a9e0234
Gabe <gc3@3633830.no-reply.drupal.org>
2020-02-07 13:41:03 -0800

    sg-891 - update backend conditionals, styling

--- web/modules/custom/sfgov_utilities/js/sfgov_utilities_services.js
+++ web/modules/custom/sfgov_utilities/js/sfgov_utilities_services.js
@@ -1,8 +1,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.sfgovUtilitiesServices = {
     attach: function (context, settings) {
-      var services = $('.sfgov-service-card');
-      if (services.length >= 12 && $(window).width() < 701) {
+      if ($(window).width() < 701) {
         $('.sfgov-services-section .sfgov-services').each(function(){
           $(this).addClass('accordion');
           $(this).accordion({
