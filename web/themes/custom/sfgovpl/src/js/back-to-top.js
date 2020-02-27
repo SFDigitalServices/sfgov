61bf22fe956e1ac7197049f04ce4c24580c1231b
Rob Decker <rob@deckerdev.com>
2020-02-27 11:48:27 -0700

    sg-943 - Enhance button visibility javascript

--- web/themes/custom/sfgovpl/src/js/back-to-top.js
+++ web/themes/custom/sfgovpl/src/js/back-to-top.js
@@ -11,21 +11,24 @@
     attach: function (context) {
       var buttonText = Drupal.t('Back to top');
       $('body', context).once('backToTop').append('<a id="back-to-top" class="back-to-top">' + buttonText + '<span /></a>');
-      var then = 0;
-      var now = 0;
-      $(window, context).once('scroll').on('scroll', function() {
-        now = $(window, context).scrollTop();
-        if (now > then && now > 1000) {
-          $('#back-to-top', context).addClass('show');
-        }
-        else {
-          $('#back-to-top', context).removeClass('show');
-        }
-        then = now;
-      });
       $('#back-to-top', context).on('click', function(e) {
         e.preventDefault();
         $('html, body', context).animate({scrollTop: 0}, '300');
+      });
+
+      $(window, context).on('load', function() {
+        var then = 0;
+        var now = 0;
+        $(window, context).once('scroll').on('scroll', function() {
+          now = $(window, context).scrollTop();
+          if (now > then && now > 1000) {
+            $('#back-to-top').addClass('show');
+          }
+          else {
+            $('#back-to-top').removeClass('show');
+          }
+          then = now;
+        });
       });
     }
   };
