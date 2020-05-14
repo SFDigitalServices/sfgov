2303d53b81919d42e1ceff14dfe2161c58bfb1dd
Thiago Regis <tregismoreira@gmail.com>
2020-05-14 20:45:47 -0300

    sg-28 - Fixes to the Instagram feed.

--- web/themes/custom/sfgovpl/src/js/instagram-feed.js
+++ web/themes/custom/sfgovpl/src/js/instagram-feed.js
@@ -8,18 +8,22 @@
       Object.keys(items).forEach(function(key) {
         var profile = items[key].instagram_profile;
         var paragraph_id = items[key].paragraph_id;
+        var feed_container =
+          '.paragraph--type--instagram-embed--' +
+          paragraph_id +
+          ' .__instagram-profile';
 
         $.instagramFeed({
           username: profile,
-          container: '.paragraph--type--instagram-embed--' + paragraph_id,
+          container: feed_container,
           display_profile: false,
           display_biography: false,
           display_gallery: true,
           callback: null,
           styling: true,
-          items: 8,
+          items: 12,
           items_per_row: 4,
-          margin: 1,
+          margin: 2,
           image_size: 150,
         });
       });
