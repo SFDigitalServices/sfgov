7c5dc4ce82a9d255d0279aaf952a3a3f2005fd67
Anthony Kong <anthony.kong@sfgov.org>
2020-06-25 11:03:57 -0700

    sg-1113 - generalize json string cleanup

--- web/modules/custom/sfgov_utilities/src/Plugin/views/style/SFGovCustomSerializer.php
+++ web/modules/custom/sfgov_utilities/src/Plugin/views/style/SFGovCustomSerializer.php
@@ -1,48 +1,57 @@
 <?php
 
-  namespace Drupal\sfgov_utilities\Plugin\views\style;
+namespace Drupal\sfgov_utilities\Plugin\views\style;
 
-  use Drupal\rest\Plugin\views\style\Serializer;
+use Drupal\rest\Plugin\views\style\Serializer;
+
+/**
+ * Custom serializer.
+ *
+ * @ViewsStyle(
+ *   id = "sfgov_custom_serializer",
+ *   title = @Translation("SF.gov custom serializer"),
+ *   help = @Translation("Serializes views row data using the Serializer component."),
+ *   display_types = {"data"}
+ * )
+ */
+class SFGovCustomSerializer extends Serializer {
+
   /**
-   * Custom serializer
-   *
-   * @ViewsStyle(
-   *   id = "sfgov_custom_serializer",
-   *   title = @Translation("SF.gov custom serializer"),
-   *   help = @Translation("Serializes views row data using the Serializer component."),
-   *   display_types = {"data"}
-   * )
+   * Custom render primarily used to clean up fields that contain JSON.
    */
-  class SFGovCustomSerializer extends Serializer {
-    public function render() {
-      $rows = [];
-      // If the Data Entity row plugin is used, this will be an array of entities
-      // which will pass through Serializer to one of the registered Normalizers,
-      // which will transform it to arrays/scalars. If the Data field row plugin
-      // is used, $rows will not contain objects and will pass directly to the
-      // Encoder.
-      foreach ($this->view->result as $row_index => $row) {
-        $this->view->row_index = $row_index;
-        $row_render = $this->view->rowPlugin->render($row);
-        foreach($row_render as $key => $value) {
-          if($key == 'field_formio_render_options') {
-            $the_row[$key] = json_decode($value, TRUE);
-          } else {
-            $the_row[$key] = $value;
-          }
+  public function render() {
+
+    $rows = [];
+    // If the Data Entity row plugin is used, this will be an array of entities
+    // which will pass through Serializer to one of the registered Normalizers,
+    // which will transform it to arrays/scalars. If the Data field row plugin
+    // is used, $rows will not contain objects and will pass directly to the
+    // Encoder.
+    foreach ($this->view->result as $row_index => $row) {
+      $this->view->row_index = $row_index;
+      $row_render = $this->view->rowPlugin->render($row);
+      foreach ($row_render as $key => $value) {
+        $json = json_decode($value, TRUE);
+        if ($json) {
+          $the_row[$key] = $json;
         }
-        $rows[] = $the_row;
+        else {
+          $the_row[$key] = $value;
+        }
       }
-  
-      unset($this->view->row_index);
-      // Get the content type configured in the display or fallback to the
-      // default.
-      if ((empty($this->view->live_preview))) {
-        $content_type = $this->displayHandler->getContentType();
-      }
-      else {
-        $content_type = !empty($this->options['formats']) ? reset($this->options['formats']) : 'json';
-      }
-      return $this->serializer->serialize($rows, $content_type, ['views_style_plugin' => $this, 'json_encode_options' => JSON_UNESCAPED_SLASHES]);
+      $rows[] = $the_row;
     }
+
+    unset($this->view->row_index);
+    // Get the content type configured in the display or fallback to the
+    // default.
+    if ((empty($this->view->live_preview))) {
+      $content_type = $this->displayHandler->getContentType();
+    }
+    else {
+      $content_type = !empty($this->options['formats']) ? reset($this->options['formats']) : 'json';
+    }
+    return $this->serializer->serialize($rows, $content_type, ['views_style_plugin' => $this, 'json_encode_options' => JSON_UNESCAPED_SLASHES]);
   }
+
+}
