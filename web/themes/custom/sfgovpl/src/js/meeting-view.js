SFDigitalServices/sfgov

commit 82daa4788a7a8852d885aa60a35f526b7b990a7d

SFDPH - Sprint 6 (#592)
* 50 - Basics for sending the "Expiring Email Notification".

* sg-50 - Schedule job

* sg-50 - module dependencies

* sg-50 - Email scheduling logic

* sg-50 - Temporarily disable sfgov_campaigns

* sg-50 - Add comment for custom methods.

* 89 - Update field Direct External URL.

* sg-89 - Fix response cache to make sure the redirect response will not be cached for authenticated users.

* sg-94 - Fix Meeting card + fix meeting list

* 59 - Fix document listing in Public Bodies

* 59 - Fix document listing in Meetings

* sg-68 - Refactor Alert on Public Bodies

* sg-102 - Fix Scheduler issue.

* sg-102 - enable sfgov_campaigns

* sg-50 - Refinements on the email notification.

* sg-102 - apply patch to fix issue with scheduler_content_moderation_integration

* sg-101 - Fix html template for email notification

* sg-101 - enable default time for Scheduler

* sg-101 - Form structure for remote meetings

* sg-101 - Update all meetings to set the "In-person" meeting location by default.

* sg-101 - Update template for remote location

* sg-101 - Fix twig condition

* sg-95 - Fixes on the mobile version for the meeting list view.

* sg-101 - Fix update script

* Rollback changes to Mail System

* Fix styles for In-Person location address field

* Fix in-person location address reference entity

web/themes/custom/sfgovpl/sfgovpl.libraries.yml
@@ -108,6 +108,14 @@ sfgov-video:
     - core/jquery.once
     - core/drupal
 
+meeting-view:
+  js:
+    src/js/meeting-view.js: {}
+  dependencies:
+    - core/jquery
+    - core/jquery.once
+    - core/drupal
+
 sfgov-campaign:
   js:
     src/js/campaign.js: {}

web/themes/custom/sfgovpl/src/js/meeting-view.js
@@ -1,0 +1,13 @@
+(function($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.meeting = {
+    attach: function(context, settings) {
+      $('.meeting-list-filters-form .form-wrapper').addClass('closed');
+      $('.meeting-list-filters-form .form-wrapper').once('meeting-view').append('<a class="meeting-list-filters-toggle"></a>');
+      $('.meeting-list-filters-toggle').once('meeting-view').click(function() {
+        $('.meeting-list-filters-form .form-wrapper').toggleClass('closed');
+      });
+    },
+  };
+})(jQuery, Drupal);

web/themes/custom/sfgovpl/src/sass/view/_view-meetings.scss
@@ -1,4 +1,8 @@
 .views--meetings {
+  .ms-parent {
+    width: 100%;
+  }
+
   .sfgov-section-container {
     @include contain-1090;
     overflow: hidden;
@@ -26,6 +30,24 @@
 
   .content {
     padding: 60px 0;
+    @media (max-width: $mobile-screen) {
+      padding-top: 0;
+    }
+  }
+
+  @media (max-width: $mobile-screen) {
+    .view-grouping {
+      padding: 60px 20px;
+      margin: 0 -20px;
+
+      &:nth-child(even) {
+        background-color: #F6F7F8;
+      }
+
+      &:last-child {
+        margin-bottom: 60px;
+      }
+    }
   }
 
   h2.group {
@@ -47,8 +69,48 @@
       overflow: inherit;
     }
 
+    .fieldset-wrapper {
+      transition: all 0.3s ease-in-out;
+      overflow: hidden;
+    }
+
+    .form-wrapper {
+      .meeting-list-filters-toggle {
+        display: none;
+      }
+      @media (max-width: $mobile-screen) {
+        .meeting-list-filters-toggle {
+          display: block;
+          &::before {
+            content: "Show less";
+            padding-left: 30px;
+            background-image: url(../../src/img/chevron-up.svg);
+            background-size: 18px;
+            background-position: left;
+            background-repeat: no-repeat;
+            color: $c-bright-blue;
+          }
+        }
+        &.closed {
+          .meeting-list-filters-toggle::before {
+            content: "Show more";
+            background-image: url(../../src/img/chevron-down.svg);
+          }
+          .fieldset-wrapper {
+            height: 0;
+            opacity: 0;
+          }
+        }
+      }
+    }
+
     legend {
-      @include fs-title-3;
+      @media (max-width: $mobile-screen) {
+        @include fs-title-4;
+      }
+      @media (min-width: $mobile-screen) {
+        @include fs-title-3;
+      }
     }
 
     form {
@@ -95,8 +157,15 @@
   }
 
   .views-row {
-    padding-left: 30px;
+    @media (min-width: $mobile-screen) {
+      padding-left: 30px;
+    }
     margin-bottom: 40px;
+    @media (max-width: $mobile-screen) {
+      &:last-child {
+        margin: 0;
+      }
+    }
 
     article {
       .article--teaser__title {
@@ -106,10 +175,18 @@
         .__footer {
           display: flex;
           margin-top: 1rem;
+          @media (max-width: $mobile-screen) {
+            flex-direction: column;
+          }
 
           .__time,
           .__address {
-            width: 50%;
+            @media (min-width: $mobile-screen) {
+              width: 50%;
+            }
+            @media (max-width: $mobile-screen) {
+              margin-bottom: 10px;
+            }
             position: relative;
             padding-left: 35px;
 

web/themes/custom/sfgovpl/templates/views/views-view--meetings--page.html.twig
@@ -30,6 +30,9 @@
  * @see template_preprocess_views_view()
  */
 #}
+
+{{ attach_library('sfgovpl/meeting-view') }}
+
 {%
   set classes = [
   dom_id ? 'js-view-dom-id-' ~ dom_id,

config/core.entity_form_display.node.meeting.default.yml
@@ -3,16 +3,21 @@ langcode: en
 status: true
 dependencies:
   config:
-    - entity_browser.browser.location_event_address
+    - entity_browser.browser.location_physical
     - field.field.node.meeting.body
+    - field.field.node.meeting.field_abstract
     - field.field.node.meeting.field_address
     - field.field.node.meeting.field_agenda
     - field.field.node.meeting.field_dept
     - field.field.node.meeting.field_direct_external_url
     - field.field.node.meeting.field_end_date
     - field.field.node.meeting.field_intro_text
+    - field.field.node.meeting.field_link
+    - field.field.node.meeting.field_location_in_person
+    - field.field.node.meeting.field_location_online
     - field.field.node.meeting.field_meeting_artifacts
     - field.field.node.meeting.field_meeting_cancel
+    - field.field.node.meeting.field_phone_numbers
     - field.field.node.meeting.field_public_body
     - field.field.node.meeting.field_regulations_accordions
     - field.field.node.meeting.field_regulations_title
@@ -41,7 +46,7 @@ third_party_settings:
         - field_meeting_cancel
         - field_start_date
         - field_end_date
-        - field_address
+        - field_direct_external_url
       parent_name: ''
       weight: 2
       format_type: fieldset
@@ -59,7 +64,7 @@ third_party_settings:
         - field_video_embed
         - field_url
       parent_name: ''
-      weight: 5
+      weight: 6
       format_type: details
       region: content
       format_settings:
@@ -74,7 +79,7 @@ third_party_settings:
         - field_regulations_title
         - field_regulations_accordions
       parent_name: ''
-      weight: 6
+      weight: 7
       format_type: fieldset
       region: content
       format_settings:
@@ -84,6 +89,38 @@ third_party_settings:
         required_fields: true
         open: false
       label: 'Regulations and accessibility'
+    group_meeting_location:
+      children:
+        - field_location_in_person
+        - field_address
+        - field_location_online
+        - group_meeting_online
+      parent_name: ''
+      weight: 3
+      format_type: fieldset
+      region: content
+      format_settings:
+        id: ''
+        classes: ''
+        description: ''
+        required_fields: true
+      label: 'Meeting location'
+    group_meeting_online:
+      children:
+        - field_abstract
+        - field_link
+        - field_phone_numbers
+      parent_name: group_meeting_location
+      weight: 5
+      format_type: details
+      region: content
+      format_settings:
+        id: ''
+        classes: ''
+        description: ''
+        open: false
+        required_fields: true
+      label: Online
 id: node.meeting.default
 targetEntityType: node
 bundle: meeting
@@ -91,7 +128,7 @@ mode: default
 content:
   body:
     type: text_textarea_with_summary
-    weight: 3
+    weight: 4
     settings:
       rows: 9
       summary_rows: 3
@@ -99,16 +136,24 @@ content:
       show_summary: false
     third_party_settings: {  }
     region: content
-  field_address:
+  field_abstract:
     weight: 5
     settings:
-      entity_browser: location_event_address
+      rows: 5
+      placeholder: ''
+    third_party_settings: {  }
+    type: string_textarea
+    region: content
+  field_address:
+    weight: 3
+    settings:
+      entity_browser: location_physical
       field_widget_display: rendered_entity
+      field_widget_display_settings:
+        view_mode: default
       field_widget_edit: true
       field_widget_remove: true
       selection_mode: selection_append
-      field_widget_display_settings:
-        view_mode: default
       field_widget_replace: false
       open: false
     third_party_settings: {  }
@@ -116,7 +161,7 @@ content:
     region: content
   field_agenda:
     type: paragraphs
-    weight: 4
+    weight: 5
     settings:
       title: Paragraph
       title_plural: Paragraphs
@@ -134,7 +179,7 @@ content:
     third_party_settings: {  }
     region: content
   field_dept:
-    weight: 8
+    weight: 9
     settings:
       match_operator: CONTAINS
       size: 60
@@ -144,7 +189,7 @@ content:
     type: entity_reference_autocomplete
     region: content
   field_direct_external_url:
-    weight: 9
+    weight: 7
     settings:
       placeholder_url: ''
       placeholder_title: ''
@@ -158,16 +203,38 @@ content:
     type: datetime_default
     region: content
   field_intro_text:
-    weight: 12
+    weight: 20
     settings:
       rows: 5
       placeholder: ''
     third_party_settings: {  }
     type: text_textarea
     region: content
+  field_link:
+    weight: 6
+    settings:
+      placeholder_url: ''
+      placeholder_title: ''
+    third_party_settings: {  }
+    type: link_default
+    region: content
+  field_location_in_person:
+    weight: 2
+    settings:
+      display_label: true
+    third_party_settings: {  }
+    type: boolean_checkbox
+    region: content
+  field_location_online:
+    weight: 4
+    settings:
+      display_label: true
+    third_party_settings: {  }
+    type: boolean_checkbox
+    region: content
   field_meeting_artifacts:
     type: paragraphs
-    weight: 7
+    weight: 8
     settings:
       title: Paragraph
       title_plural: Paragraphs
@@ -192,16 +259,23 @@ content:
     type: boolean_checkbox
     region: content
   field_phone_numbers:
-    weight: 11
+    weight: 7
     settings:
       title: Paragraph
       title_plural: Paragraphs
-      edit_mode: open
+      edit_mode: closed
+      closed_mode: summary
+      autocollapse: none
+      closed_mode_threshold: 0
       add_mode: dropdown
       form_display_mode: default
       default_paragraph_type: _none
+      features:
+        add_above: '0'
+        collapse_edit_all: collapse_edit_all
+        duplicate: duplicate
     third_party_settings: {  }
-    type: entity_reference_paragraphs
+    type: paragraphs
     region: content
   field_public_body:
     weight: 1
@@ -215,7 +289,7 @@ content:
     region: content
   field_regulations_accordions:
     type: entity_reference_paragraphs
-    weight: 19
+    weight: 20
     settings:
       title: Paragraph
       title_plural: Paragraphs
@@ -240,7 +314,7 @@ content:
     type: datetime_default
     region: content
   field_url:
-    weight: 15
+    weight: 24
     settings:
       placeholder_url: ''
       placeholder_title: ''
@@ -248,13 +322,13 @@ content:
     type: link_default
     region: content
   field_video_embed:
-    weight: 14
+    weight: 23
     settings: {  }
     third_party_settings: {  }
     type: video_embed_field_textfield
     region: content
   field_video_option:
-    weight: 13
+    weight: 22
     settings: {  }
     third_party_settings: {  }
     type: options_buttons

config/core.entity_form_display.node.public_body.default.yml
@@ -6,8 +6,9 @@ dependencies:
     - entity_browser.browser.location_physical
     - field.field.node.public_body.field_about_description
     - field.field.node.public_body.field_address
+    - field.field.node.public_body.field_alert_expiration_date
+    - field.field.node.public_body.field_alert_text
     - field.field.node.public_body.field_board_members
-    - field.field.node.public_body.field_callout
     - field.field.node.public_body.field_departments
     - field.field.node.public_body.field_description
     - field.field.node.public_body.field_email
@@ -20,6 +21,7 @@ dependencies:
   module:
     - allowed_formats
     - content_moderation
+    - datetime
     - entity_browser
     - field_group
     - maxlength
@@ -36,7 +38,7 @@ third_party_settings:
         - field_phone_numbers
         - field_email
       parent_name: ''
-      weight: 8
+      weight: 9
       format_type: details
       region: content
       format_settings:
@@ -46,6 +48,21 @@ third_party_settings:
         open: false
         required_fields: true
       label: Contact
+    group_alert:
+      children:
+        - field_alert_text
+        - field_alert_expiration_date
+      parent_name: ''
+      weight: 3
+      format_type: details
+      region: content
+      format_settings:
+        id: ''
+        classes: ''
+        description: ''
+        open: false
+        required_fields: true
+      label: Alert
 id: node.public_body.default
 targetEntityType: node
 bundle: public_body
@@ -53,12 +70,12 @@ mode: default
 content:
   created:
     type: datetime_timestamp
-    weight: 21
+    weight: 22
     region: content
     settings: {  }
     third_party_settings: {  }
   field_about_description:
-    weight: 5
+    weight: 6
     settings:
       rows: 5
       placeholder: ''
@@ -80,9 +97,26 @@ content:
     third_party_settings: {  }
     type: entity_browser_entity_reference
     region: content
+  field_alert_expiration_date:
+    weight: 24
+    settings:
+      increment: 15
+      date_order: YMD
+      time_type: '24'
+    third_party_settings: {  }
+    type: datetime_datelist
+    region: content
+  field_alert_text:
+    weight: 23
+    settings:
+      rows: 5
+      placeholder: ''
+    third_party_settings: {  }
+    type: text_textarea
+    region: content
   field_board_members:
     type: paragraphs
-    weight: 6
+    weight: 7
     settings:
       title: Paragraph
       title_plural: Paragraphs
@@ -99,27 +133,8 @@ content:
         duplicate: duplicate
     third_party_settings: {  }
     region: content
-  field_callout:
-    type: paragraphs
-    weight: 3
-    settings:
-      title: Callout
-      title_plural: Callouts
-      edit_mode: closed
-      closed_mode: preview
-      autocollapse: none
-      closed_mode_threshold: 0
-      add_mode: button
-      form_display_mode: default
-      default_paragraph_type: _none
-      features:
-        add_above: '0'
-        collapse_edit_all: collapse_edit_all
-        duplicate: duplicate
-    third_party_settings: {  }
-    region: content
   field_departments:
-    weight: 9
+    weight: 10
     settings:
       match_operator: CONTAINS
       size: 60
@@ -165,7 +180,7 @@ content:
     region: content
   field_other_info:
     type: paragraphs
-    weight: 7
+    weight: 8
     settings:
       title: Section
       title_plural: Sections
@@ -213,7 +228,7 @@ content:
     region: content
   field_public_body_meetings:
     type: paragraphs
-    weight: 4
+    weight: 5
     settings:
       title: Paragraph
       title_plural: Paragraphs
@@ -232,13 +247,13 @@ content:
     region: content
   moderation_state:
     type: moderation_state_default
-    weight: 20
+    weight: 21
     settings: {  }
     region: content
     third_party_settings: {  }
   path:
     type: path
-    weight: 16
+    weight: 17
     region: content
     settings: {  }
     third_party_settings: {  }
@@ -246,18 +261,18 @@ content:
     type: boolean_checkbox
     settings:
       display_label: true
-    weight: 14
+    weight: 15
     region: content
     third_party_settings: {  }
   publish_on:
     type: datetime_timestamp_no_default
-    weight: 12
+    weight: 13
     region: content
     settings: {  }
     third_party_settings: {  }
   publish_state:
     type: scheduler_moderation
-    weight: 10
+    weight: 11
     region: content
     settings: {  }
     third_party_settings: {  }
@@ -265,14 +280,14 @@ content:
     type: boolean_checkbox
     settings:
       display_label: true
-    weight: 17
+    weight: 18
     region: content
     third_party_settings: {  }
   sticky:
     type: boolean_checkbox
     settings:
       display_label: true
-    weight: 15
+    weight: 16
     region: content
     third_party_settings: {  }
   title:
@@ -285,7 +300,7 @@ content:
     third_party_settings: {  }
   uid:
     type: entity_reference_autocomplete
-    weight: 18
+    weight: 19
     settings:
       match_operator: CONTAINS
       size: 60
@@ -295,18 +310,18 @@ content:
     third_party_settings: {  }
   unpublish_on:
     type: datetime_timestamp_no_default
-    weight: 11
+    weight: 12
     region: content
     settings: {  }
     third_party_settings: {  }
   unpublish_state:
     type: scheduler_moderation
-    weight: 13
+    weight: 14
     region: content
     settings: {  }
     third_party_settings: {  }
   url_redirects:
-    weight: 19
+    weight: 20
     region: content
     settings: {  }
     third_party_settings: {  }

config/core.entity_view_display.node.meeting.card.yml
@@ -5,14 +5,19 @@ dependencies:
   config:
     - core.entity_view_mode.node.card
     - field.field.node.meeting.body
+    - field.field.node.meeting.field_abstract
     - field.field.node.meeting.field_address
     - field.field.node.meeting.field_agenda
     - field.field.node.meeting.field_dept
     - field.field.node.meeting.field_direct_external_url
     - field.field.node.meeting.field_end_date
     - field.field.node.meeting.field_intro_text
+    - field.field.node.meeting.field_link
+    - field.field.node.meeting.field_location_in_person
+    - field.field.node.meeting.field_location_online
     - field.field.node.meeting.field_meeting_artifacts
     - field.field.node.meeting.field_meeting_cancel
+    - field.field.node.meeting.field_phone_numbers
     - field.field.node.meeting.field_public_body
     - field.field.node.meeting.field_regulations_accordions
     - field.field.node.meeting.field_regulations_title
@@ -23,6 +28,7 @@ dependencies:
     - node.type.meeting
   module:
     - datetime
+    - entity_reference_revisions
     - user
 id: node.meeting.card
 targetEntityType: node
@@ -43,19 +49,23 @@ content:
     label: hidden
     settings:
       timezone_override: ''
-      date_format: 'l, M 4, Y - h:ia'
+      date_format: 'l, M j, Y - h:ia'
     third_party_settings: {  }
     type: datetime_custom
     region: content
 hidden:
   body: true
   content_moderation_control: true
+  field_abstract: true
   field_address: true
   field_agenda: true
   field_dept: true
   field_direct_external_url: true
   field_end_date: true
   field_intro_text: true
+  field_link: true
+  field_location_in_person: true
+  field_location_online: true
   field_meeting_artifacts: true
   field_meeting_cancel: true
   field_public_body: true

config/core.entity_view_display.node.meeting.default.yml
@@ -4,14 +4,19 @@ status: true
 dependencies:
   config:
     - field.field.node.meeting.body
+    - field.field.node.meeting.field_abstract
     - field.field.node.meeting.field_address
     - field.field.node.meeting.field_agenda
     - field.field.node.meeting.field_dept
     - field.field.node.meeting.field_direct_external_url
     - field.field.node.meeting.field_end_date
     - field.field.node.meeting.field_intro_text
+    - field.field.node.meeting.field_link
+    - field.field.node.meeting.field_location_in_person
+    - field.field.node.meeting.field_location_online
     - field.field.node.meeting.field_meeting_artifacts
     - field.field.node.meeting.field_meeting_cancel
+    - field.field.node.meeting.field_phone_numbers
     - field.field.node.meeting.field_public_body
     - field.field.node.meeting.field_regulations_accordions
     - field.field.node.meeting.field_regulations_title
@@ -45,6 +50,13 @@ content:
     region: content
     settings: {  }
     third_party_settings: {  }
+  field_abstract:
+    weight: 114
+    label: hidden
+    settings: {  }
+    third_party_settings: {  }
+    type: basic_string
+    region: content
   field_address:
     weight: 2
     label: hidden
@@ -99,6 +111,38 @@ content:
     third_party_settings: {  }
     type: text_default
     region: content
+  field_link:
+    weight: 115
+    label: hidden
+    settings:
+      trim_length: 80
+      url_only: false
+      url_plain: false
+      rel: ''
+      target: ''
+    third_party_settings: {  }
+    type: link
+    region: content
+  field_location_in_person:
+    weight: 113
+    label: hidden
+    settings:
+      format: default
+      format_custom_false: ''
+      format_custom_true: ''
+    third_party_settings: {  }
+    type: boolean
+    region: content
+  field_location_online:
+    weight: 112
+    label: hidden
+    settings:
+      format: default
+      format_custom_false: ''
+      format_custom_true: ''
+    third_party_settings: {  }
+    type: boolean
+    region: content
   field_meeting_artifacts:
     type: entity_reference_revisions_entity_view
     weight: 9
@@ -119,8 +163,8 @@ content:
     type: boolean
     region: content
   field_phone_numbers:
-    weight: 109
-    label: above
+    weight: 116
+    label: hidden
     settings:
       view_mode: default
       link: ''

commit 7da53bdfcca761380f96dea1fc1af9f1ca368c82

[sg-1153] Fix conflict-related issues.
Caused by #592 and #606 working on the same filters.

web/themes/custom/sfgovpl/sfgovpl.info.yml
@@ -20,7 +20,6 @@ libraries:
   - sfgovpl/sfgov-components
   - sfgovpl/sfgov-back-to-top
   - sfgovpl/sfgov-in-this-page
-  - sfgovpl/sfgov-campaign
 
 libraries-override:
   system/base:

web/themes/custom/sfgovpl/sfgovpl.libraries.yml
@@ -108,14 +108,6 @@ sfgov-video:
     - core/jquery.once
     - core/drupal
 
-meeting-view:
-  js:
-    src/js/meeting-view.js: {}
-  dependencies:
-    - core/jquery
-    - core/jquery.once
-    - core/drupal
-
 sfgov-campaign:
   js:
     src/js/campaign.js: {}

web/themes/custom/sfgovpl/src/js/meeting-view.js
@@ -1,13 +1,0 @@
-(function($, Drupal) {
-  'use strict';
-
-  Drupal.behaviors.meeting = {
-    attach: function(context, settings) {
-      $('.meeting-list-filters-form .form-wrapper').addClass('closed');
-      $('.meeting-list-filters-form .form-wrapper').once('meeting-view').append('<a class="meeting-list-filters-toggle"></a>');
-      $('.meeting-list-filters-toggle').once('meeting-view').click(function() {
-        $('.meeting-list-filters-form .form-wrapper').toggleClass('closed');
-      });
-    },
-  };
-})(jQuery, Drupal);

web/themes/custom/sfgovpl/src/sass/view/_view-meetings.scss
@@ -1,10 +1,6 @@
 @import 'view-meetings-filters';
 
 .views--meetings {
-  .ms-parent {
-    width: 100%;
-  }
-
   .sfgov-section-container {
     @include contain-1090;
     overflow: hidden;

web/themes/custom/sfgovpl/templates/views/views-view--meetings--page.html.twig
@@ -30,9 +30,6 @@
  * @see template_preprocess_views_view()
  */
 #}
-
-{{ attach_library('sfgovpl/meeting-view') }}
-
 {%
   set classes = [
   dom_id ? 'js-view-dom-id-' ~ dom_id,