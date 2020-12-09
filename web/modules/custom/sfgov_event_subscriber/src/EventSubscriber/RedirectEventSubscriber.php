b49cacf9dd15d16469ff6a8c75757f5ce0415553
ant <anthony.kong@sfgov.org>
2020-12-09 13:39:36 -0800

    Sg 1251 clear cache redirect (#760)
    * SG-1254 - clear dynamic page cache on node update
    
    * SG-1254 - prevent caching for redirect
    
    * SG-1254 - return if node is unpublished
    
    * SG-1251 - remove unused things

--- web/modules/custom/sfgov_event_subscriber/src/EventSubscriber/RedirectEventSubscriber.php
+++ web/modules/custom/sfgov_event_subscriber/src/EventSubscriber/RedirectEventSubscriber.php
@@ -11,7 +11,6 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Drupal\Core\Routing\TrustedRedirectResponse;
-use Drupal\Core\Session\AccountInterface;
 
 /**
  * Event Subscriber RedirectEventSubscriber.
@@ -36,40 +35,44 @@ class RedirectEventSubscriber implements EventSubscriberInterface {
     }
 
     $redirect_url = NULL;
+    $cacheableDependency = NULL;
     $node = $event->getRequest()->attributes->get('node');
     $media = $event->getRequest()->attributes->get('media');
 
-    if($node && $node->isPublished()) {
-      // Add cache context to make sure the request won't be cached for authenticated users.
-      $node->addCacheContexts(['user.roles:anonymous']);
-
-      // Get node type.
-      $node_type = strtolower($node->type->entity->label());
-
-      // Redirect rule based on the field `field_direct_external_url`.
-      if ($node->hasField('field_direct_external_url') ){
-        $field_external_url = $node->get('field_direct_external_url')->getValue();
-
-        if (!empty($field_external_url[0]) && $field_external_url[0]['uri'] != ''){
-          // This is where you set the destination.
-          $redirect_url = $field_external_url[0]['uri'];
-          $response = new TrustedRedirectResponse($redirect_url);
-          $response->addCacheableDependency($node);
-          $event->setResponse($response);
-        }
+    if($node) {
+      if(!$node->isPublished()) {
+        return;
       }
+      else {
+        // Add cache context to make sure the request won't be cached for authenticated users.
+        $node->addCacheContexts(['user.roles:anonymous']);
 
-      if ($node_type == 'department' && $node->hasField('field_go_to_current_url')) {
-        $field_go_to_current_url = $node->get('field_go_to_current_url')->getValue();
+        // Get node type.
+        $node_type = strtolower($node->type->entity->label());
 
-        if (!empty($field_go_to_current_url[0]) && $field_go_to_current_url[0]['value'] == '1') {
-          $field_dept_url = $node->get('field_url')->getValue();
+        // Redirect rule based on the field `field_direct_external_url`.
+        if ($node->hasField('field_direct_external_url') ){
+          $field_external_url = $node->get('field_direct_external_url')->getValue();
 
-          if (!empty($field_dept_url[0]) && $field_dept_url[0]['uri'] != '') {
-            $redirect_url = $field_dept_url[0]['uri'];
-            $response = new TrustedRedirectResponse($redirect_url);
-            $response->addCacheableDependency($node);
-            $event->setResponse($response);
+          if (!empty($field_external_url[0]) && $field_external_url[0]['uri'] != ''){
+            // This is where you set the destination.
+            $redirect_url = $field_external_url[0]['uri'];
+            $cacheableDependency = $node;
+          }
+        }
+
+        if ($node_type == 'department' && $node->hasField('field_go_to_current_url')) {
+          $redirect_url = NULL;
+          $cacheableDependency = NULL;
+          $field_go_to_current_url = $node->get('field_go_to_current_url')->getValue();
+
+          if (!empty($field_go_to_current_url[0]) && $field_go_to_current_url[0]['value'] == '1') {
+            $field_dept_url = $node->get('field_url')->getValue();
+
+            if (!empty($field_dept_url[0]) && $field_dept_url[0]['uri'] != '') {
+              $redirect_url = $field_dept_url[0]['uri'];
+              $cacheableDependency = $node;
+            }
           }
         }
       }
@@ -81,7 +84,6 @@ class RedirectEventSubscriber implements EventSubscriberInterface {
         if(!empty($field_file)) {
           $file_id = $field_file[0]['target_id'];
           $file_url = \Drupal\file\Entity\File::load($file_id)->url();
-          error_log($file_url);
           $redirect_url = $file_url;
         }
         else if(!empty($field_doc_url)) {
@@ -91,7 +93,14 @@ class RedirectEventSubscriber implements EventSubscriberInterface {
     }
 
     if(!empty($redirect_url)) {
-      $response = new TrustedRedirectResponse($redirect_url);
+      $response_headers = [
+        'Cache-Control' => 'no-cache, no-store, must-revalidate',
+      ];
+      $response = new TrustedRedirectResponse($redirect_url, '302', $response_headers);
+      if(!empty($cacheableDependency)) {
+        $response->addCacheableDependency($cacheableDependency);
+      }
+      \Drupal::service('page_cache_kill_switch')->trigger(); // disable page cache for anonymous requests
       $event->setResponse($response);
     }
 
