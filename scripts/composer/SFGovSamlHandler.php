112dc7abdfd654c2f5e3a6ecf3bbba818c3ba25a
Anthony Kong <anthony.kong@sfgov.org>
2021-02-12 01:01:46 -0800

    SG-1318 - copy necessary saml files

--- scripts/composer/SFGovSamlHandler.php
+++ scripts/composer/SFGovSamlHandler.php
@@ -3,9 +3,6 @@
 namespace SFGovSaml;
 
 use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Finder\Finder;
 
 class SFGovSamlHandler
 {
@@ -19,17 +16,17 @@ class SFGovSamlHandler
     $root = static::getDrupalRoot(getcwd());
     $vendor_dir = $event->getComposer()->getConfig()->get('vendor-dir');
 
-    $config_dir_source = $root . '/private/saml/config/';
+    $config_dir_source = $root . '/private/saml/config/.';
     $config_dir_dest = $vendor_dir . '/simplesamlphp/simplesamlphp/config';
 
     $config_file_source = $root . '/private/saml/simplesaml_config.php';
     $config_file_dest = $vendor_dir . '/simplesamlphp/simplesamlphp/config/config.php';
 
-    $metadata_dir_source = $root . '/private/saml/metadata/';
+    $metadata_dir_source = $root . '/private/saml/metadata/.';
     $metadata_dir_dest = $vendor_dir . '/simplesamlphp/simplesamlphp/metadata';
 
-    exec('cp -r ' . $config_dir_source . ' ' . $config_dir_dest);
+    exec(escapeshellcmd('cp -a ' . $config_dir_source . ' ' . $config_dir_dest));
     copy($config_file_source, $config_file_dest);
-    exec('cp -r ' . $metadata_dir_source . ' ' . $metadata_dir_dest);
+    exec(escapeshellcmd('cp -a ' . $metadata_dir_source . ' ' . $metadata_dir_dest));
   }
 }
