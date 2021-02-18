316e35afccac237a79a8537cfb5158c5527904ca
zakiya <zakiya@chapterthree.com>
2021-02-18 10:22:40 -0800

    Vaccine page - Real data! (and code cleanup)

--- web/modules/custom/sfgov_vaccine/src/Controller/VaccineController.php
+++ web/modules/custom/sfgov_vaccine/src/Controller/VaccineController.php
@@ -28,33 +28,40 @@ class VaccineController extends ControllerBase {
     return $instance;
   }
 
-  public function makeTitle() {
+  private function makeTitle() {
     // @todo Create admin form.
     return t("COVID-19 vaccine sites");
   }
 
-  public function makeFilters() {
-    return \Drupal::formBuilder()->getForm('\Drupal\sfgov_vaccine\Form\FilterSitesForm');
-  }
-
-  public function makeResults() {
-
+  private function datafecth() {
     // @todo Figure out what to do if this fails.
     /** @var \GuzzleHttp\Client $client */
     $client = \Drupal::service('http_client_factory')->fromOptions([
-      'base_uri' => 'http://zakiyadesigns.com/',
+      'base_uri' => 'https://vaccination-site-microservice-git-api-spec.sfds.vercel.app/',
     ]);
-    $response = $client->get('sites.json');
-    $sites = Json::decode($response->getBody());
+    $response = $client->get('api/v1/sites');
+    return Json::decode($response->getBody());
+  }
+
+  private function makeFilters() {
+    return \Drupal::formBuilder()->getForm('\Drupal\sfgov_vaccine\Form\FilterSitesForm');
+  }
+
+  private function makeResults() {
+
+    $alldata = $this->datafecth();
+
+    $sites = $alldata['data']['sites'];
 
     $results = [];
     foreach ($sites as $site_id => $site_data ) {
       // Prep some vars.
-      $available = $site_data['appointments']['available'];
+      $available = $site_data['active']; // Boolean.
+      $site_name = $site_data['name'];
 
       // Map results.
       $result = [
-        'site_name' => $site_data['site_name'],
+        'site_name' => $site_name,
         'available' => $available ? t('Appointments Available') : '',
         'attributes' => new Attribute([
           'class' => ['sfgov-service-card'],
@@ -73,13 +80,13 @@ class VaccineController extends ControllerBase {
    * @return array
    *   Return Render Array.
    */
-  public function showResults() {
+  public function displayPage() {
     return [
+      '#cache' => ['max-age' => 0,],
       '#theme' => 'vaccine-widget',
       '#page_title' => $this->makeTitle(),
       '#filters' => $this->makeFilters(),
       '#results' => $this->makeResults(),
       ];
   }
-
 }
