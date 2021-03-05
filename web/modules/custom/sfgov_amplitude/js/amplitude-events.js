e831a8d1351f0e7dad34ab802557391f52435d4d
Anthony Kong <anthony.kong@sfgov.org>
2021-03-05 14:27:23 -0800

    vax-94 - only execute if vaccine filter form exists

--- web/modules/custom/sfgov_amplitude/js/amplitude-events.js
+++ web/modules/custom/sfgov_amplitude/js/amplitude-events.js
@@ -1,27 +1,30 @@
 'use strict'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const vaccineFilterForm = document.querySelector('#vaccine-filter-form')
-  const vaccineFilterApplyButton = vaccineFilterForm.querySelector('#edit-submit')
   
   document.addEventListener('click', (event) => {
     let target = event.target
+    const vaccineFilterForm = document.querySelector('#vaccine-filter-form')
+    
+    if (vaccineFilterForm) {
+      const vaccineFilterApplyButton = vaccineFilterForm.querySelector('#edit-submit')
 
-    if (target === vaccineFilterApplyButton) {
-      const filterChecks = vaccineFilterForm.querySelectorAll('input:checked')
-      const filterSelects = vaccineFilterForm.querySelectorAll('select')
-      const filters = []
-      
-      for (let i = 0; i < filterChecks.length; i++) {
-        filters.push(filterChecks[i].getAttribute('name'))
-      }
-
-      for(let i = 0; i < filterSelects.length; i++) {
-        const filterSelect = filterSelects[i]
-        filters.push(filterSelect.value + '|' + filterSelect.options[filterSelect.selectedIndex].text)
-      }
-
-      amplitude.getInstance().logEvent('vaccine-sites-filter-click', { filters })
+      if (target === vaccineFilterApplyButton) {
+        const filterChecks = vaccineFilterForm.querySelectorAll('input:checked')
+        const filterSelects = vaccineFilterForm.querySelectorAll('select')
+        const filters = []
+        
+        for (let i = 0; i < filterChecks.length; i++) {
+          filters.push(filterChecks[i].getAttribute('name'))
+        }
+  
+        for(let i = 0; i < filterSelects.length; i++) {
+          const filterSelect = filterSelects[i]
+          filters.push(filterSelect.value + '|' + filterSelect.options[filterSelect.selectedIndex].text)
+        }
+  
+        amplitude.getInstance().logEvent('vaccine-sites-filter-click', { filters })
+      } 
     }
   })
 
