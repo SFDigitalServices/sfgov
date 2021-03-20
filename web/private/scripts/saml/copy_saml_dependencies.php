32ca4a6507ffa19b2217af92194e78c8530cb53f
Anthony Kong <anthony.kong@sfgov.org>
2021-03-20 02:24:47 -0700

    move saml dependencies script to a location that terminus can access

---
+++ web/private/scripts/saml/copy_saml_dependencies.php
@@ -0,0 +1,32 @@
+<?php
+
+  $root = defined('DRUPAL_ROOT') ? DRUPAL_ROOT : getcwd() . '/web';
+  $vendor_dir = $root . '/../vendor';
+  $sso_dir = $root . '/sites/default/files/private/saml/test';
+
+  if (isset($_ENV['TERMINUS_ENV']) && $_ENV['TERMINUS_ENV'] == 'live') {
+    echo 'terminus environment:' . $_ENV['TERMINUS_ENV'];
+    $sso_dir = $root . '/sites/default/files/private/saml/live';
+  }    
+
+  $config_dir_source = $sso_dir . '/config';
+  $config_dir_dest = $vendor_dir . '/simplesamlphp/simplesamlphp/config';
+
+  $metadata_dir_source = $sso_dir . '/metadata';
+  $metadata_dir_dest = $vendor_dir . '/simplesamlphp/simplesamlphp/metadata';
+
+  $config_exists = file_exists($config_dir_source);
+  $metadata_exists = file_exists($metadata_dir_source);
+
+  if ($config_exists && $metadata_exists) {
+    exec(escapeshellcmd('cp -a ' . $config_dir_source . '/. ' . $config_dir_dest));
+    exec(escapeshellcmd('cp -a ' . $metadata_dir_source . '/. ' . $metadata_dir_dest));
+  } else { // debug
+    echo "Files not found\n";
+    echo "config_dir_source: " . $config_dir_source . "\n";
+    echo "config_dir_dest: " . $config_dir_dest . "\n";
+    echo "metadata_dir_source: " . $metadata_dir_source . "\n";
+    echo "metadata_dir_dest: " . $metadata_dir_dest . "\n";
+    echo "config: " . $config_exists . "\n";
+    echo "metadata: " . $metadata_exists . "\n";
+  }
