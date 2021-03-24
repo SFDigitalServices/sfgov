f740ddbd328c776828f7b2286d95046bccc8ea0c
zakiya <zakiya@chapterthree.com>
2021-03-24 11:14:40 -0700

    [VAX-165] Location autocomplete.

---
+++ web/modules/custom/sfgov_vaccine/js/location_autocomplete.js
@@ -0,0 +1,32 @@
+// Reference: https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete
+
+function locationAutocomplete() {
+  const input = document.getElementById("edit-location");
+  const options = {
+    componentRestrictions: { country: "us" },
+    fields: ["geometry", "name", "formatted_address"],
+    origin: { lat: 37.7576792, lng: -122.5078107 },
+    strictBounds: false,
+  };
+  const autocomplete = new google.maps.places.Autocomplete(input, options);
+  const infowindow = new google.maps.InfoWindow();
+  const infowindowContent = document.getElementById("infowindow-content");
+  infowindow.setContent(infowindowContent);
+
+  autocomplete.addListener("place_changed", () => {
+    infowindow.close();
+    const place = autocomplete.getPlace();
+
+    input.setAttribute("data-lat", place.geometry.location.lat());
+    input.setAttribute("data-lng", place.geometry.location.lng());
+
+    if (!place.geometry || !place.geometry.location) {
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+
+    infowindowContent.children["place-name"].textContent = place.name;
+    infowindowContent.children["place-address"].textContent =
+      place.formatted_address;
+  });
+}
