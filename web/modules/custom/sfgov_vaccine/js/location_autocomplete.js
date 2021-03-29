39ca8a41a0ecb0db914391aa0109e3307caca759
zakiya <zakiya@chapterthree.com>
2021-03-29 14:57:45 -0700

    [VAX-165] Add bias for locations in the Bay Area.

--- web/modules/custom/sfgov_vaccine/js/location_autocomplete.js
+++ web/modules/custom/sfgov_vaccine/js/location_autocomplete.js
@@ -2,24 +2,23 @@
 
 function locationAutocomplete() {
   const input = document.getElementById("edit-location");
+  const neBound = new google.maps.LatLng(38.015782, -121.5566);
+  const swBound = new google.maps.LatLng(37.003, -122.928461);
+  const bayAreaBounds = new google.maps.LatLngBounds(swBound, neBound);
   const options = {
-    componentRestrictions: { country: "us" },
+    componentRestrictions: { country: "US" },
     fields: ["geometry", "name"],
-    origin: { lat: 37.7576792, lng: -122.5078107 },
     strictBounds: false,
+    bounds: bayAreaBounds,
   };
   const autocomplete = new google.maps.places.Autocomplete(input, options);
-  const infowindow = new google.maps.InfoWindow();
+  const infoWindow = new google.maps.InfoWindow();
 
   autocomplete.addListener("place_changed", () => {
-    infowindow.close();
+    infoWindow.close();
     const place = autocomplete.getPlace();
 
     input.setAttribute("data-lat", place.geometry.location.lat());
     input.setAttribute("data-lng", place.geometry.location.lng());
-
-    if (!place.geometry || !place.geometry.location) {
-      window.alert("No details available for input: '" + place.name + "'");
-    }
   });
 }
