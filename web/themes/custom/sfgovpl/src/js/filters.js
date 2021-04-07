6c2c3da91f739f95fa90edee2d25a01f6aaa9c84
zakiya <zakiya@chapterthree.com>
2021-04-07 16:32:49 -0700

    [VAX-165] Move show/hide toggle buttons outside of the form in the dom so it isn't triggered while navigating with a mouse.

--- web/themes/custom/sfgovpl/src/js/filters.js
+++ web/themes/custom/sfgovpl/src/js/filters.js
@@ -3,22 +3,24 @@
 
   Drupal.behaviors.meetings = {
     attach: function (context) {
+      const $form = $('.sfgov-filters form')
+
+      // Note: that we have two buttons instead of one because the visual
+      // placement of button changes depending on state which would produce a
+      // confusing tab order with only one button.
+      const $buttonShow = $('<button />')
+        .addClass('filter-toggle filter-toggle--show')
+        .append('<span>' + Drupal.t('Show filters') + '</span>')
+
+      const $buttonHide = $('<button />')
+        .addClass('filter-toggle filter-toggle--hide')
+        .append('<span>' + Drupal.t('Hide filters') + '</span>')
+
       $('[data-filter-toggle-content]', context).each(function () {
-        var $content = $(this)
+        const $content = $(this)
 
-        // Note: that we have two buttons instead of one because the visual
-        // placement of button changes depending on state which would produce a
-        // confusing tab order with only one button.
-        var $buttonShow = $('<button />')
-          .addClass('filter-toggle filter-toggle--show')
-          .append('<span>' + Drupal.t('Show filters') + '</span>')
-
-        var $buttonHide = $('<button />')
-          .addClass('filter-toggle filter-toggle--hide')
-          .append('<span>' + Drupal.t('Hide filters') + '</span>')
-
-        $buttonShow.insertBefore($content).once()
-        $buttonHide.insertAfter($content).once()
+        $buttonShow.insertBefore($form).once()
+        $buttonHide.insertAfter($form).once()
 
         // Handle click events.
         $buttonHide.on('click', function (e) {
@@ -38,11 +40,15 @@
 
         function respondToBreakpoint(breakpoint) {
           if (breakpoint.matches) {
+            // > $medium-screen
             toggleFilters($content, 'show')
             $buttonShow.hide()
             $buttonHide.hide()
+            moveFilterTitle('medium-plus')
           } else {
+            // < $medium-screen
             toggleFilters($content, 'hide')
+            moveFilterTitle()
           }
         }
 
@@ -52,23 +58,36 @@
         breakpoint.addListener(respondToBreakpoint)
       })
 
-      function toggleFilters($content, status) {
-        var $container = $content.parents('[data-filter-toggle-container]')
-        var $buttonHide = $container.find('.filter-toggle--hide')
-        var $buttonShow = $container.find('.filter-toggle--show')
+      function moveFilterTitle(size) {
+        const $containerAttrStr = '[data-drupal-selector="edit-container"]'
+        const $label = $form.find($containerAttrStr + ' > legend')
 
-        $container.removeClass('is-expanded is-collapsed')
+        // Adding class for css.
+        $label.addClass('sfgov-filters-legend')
+
+        if (size === 'medium-plus') {
+          // Restore original location.
+          $form.find($containerAttrStr).prepend($label)
+        } else {
+          // Move label.
+          $label.insertBefore($buttonShow)
+        }
+      }
+
+      function toggleFilters($content, status) {
+        const $filter_container = $content.parents('.sfgov-filters')
+        $filter_container.removeClass('is-expanded is-collapsed')
 
         if (status === 'hide') {
           $content.attr({ hidden: '', 'aria-hidden': true })
           $buttonShow.attr('aria-expanded', false).show()
           $buttonHide.attr('aria-expanded', true).hide()
-          $container.addClass('is-collapsed')
+          $filter_container.addClass('is-collapsed')
         } else {
           $content.removeAttr('aria-hidden hidden')
           $buttonShow.attr('aria-expanded', true).hide()
           $buttonHide.attr('aria-expanded', false).show()
-          $container.addClass('is-expanded')
+          $filter_container.addClass('is-expanded')
         }
       }
     }
