76b9411db865d6b4ff7eb409c4799ca97ff5827d
zakiya <zakiya@chapterthree.com>
2021-05-18 18:17:29 -0700

    [SG-1143] Added a latest moderation state column to admin/content

---
+++ web/modules/custom/sfgov_moderation/src/Plugin/views/field/LatestModerationState.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\sfgov_moderation\Plugin\views\field;
+
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ *
+ * @ViewsField("latest_moderation_state")
+ */
+class LatestModerationState extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+
+    $node = $this->getEntity($values);
+    return $this->nidToModeration($node->id());
+
+  }
+
+  private function nidToModeration(string $nid) {
+    $vid = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->getLatestRevisionId($nid);
+
+    $revision = \Drupal::entityTypeManager()
+      ->getStorage('node')
+      ->loadRevision($vid);
+
+    $state = $revision->get('moderation_state')->getValue();
+
+    return isset($state[0]['value']) ? $state[0]['value'] : '';
+  }
+}
