b9de94327f34006c6e67e1fcafd39252519313e4
zakiya <zakiya@chapterthree.com>
2021-05-24 20:54:21 -0700

    [SG-1143] Update reviewer options on transactions after parent departments are selected.

--- web/modules/custom/sfgov_departments/js/parent-departments.js
+++ web/modules/custom/sfgov_departments/js/parent-departments.js
@@ -1,4 +1,19 @@
 (function ($) {
+  Drupal.behaviors.get_reviewers_for_transactions = {
+    attach: function (context, settings) {
+      $(document)
+        .once("getReviewers")
+        .ajaxComplete(function (event, xhr, settings) {
+          if (
+            settings.extraData._triggering_element_name ===
+            "sfgov_department_fetch"
+          ) {
+            let $el = $('[name="sfgov_reviewer_fetch"]');
+            $el.click();
+          }
+        });
+    },
+  };
   Drupal.behaviors.parent_departments = {
     attach: function (context, settings) {
       $('.field--name-field-departments').find('input[type="text"]').once('departments').on('autocompleteclose', function(event, node) {
