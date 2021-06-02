b1d7679c4325262e7788e5324f2df9a5f5010e65
zakiya <zakiya@chapterthree.com>
2021-06-02 12:29:49 -0700

    [SG-1143] Get the review for the admin/content view.

--- web/modules/custom/sfgov_moderation/src/Plugin/views/field/LatestModerationState.php
+++ web/modules/custom/sfgov_moderation/src/Plugin/views/field/LatestModerationState.php
@@ -4,8 +4,10 @@ namespace Drupal\sfgov_moderation\Plugin\views\field;
 
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
+use Drupal\user\Entity\User;
 
 /**
+ * Field showing moderation details about the latest revision.
  *
  * @ViewsField("latest_moderation_state")
  */
@@ -17,25 +19,60 @@ class LatestModerationState extends FieldPluginBase {
   public function render(ResultRow $values) {
 
     $node = $this->getEntity($values);
-    return $this->nidToModeration($node->id());
+    $moderation_values = $this->nidToModerationField($node->id());
+
+    $render_array = [];
+    foreach ($moderation_values as $key => $moderation_value) {
+      $line = [
+        $key => [
+          '#type' => 'html_tag',
+          '#tag' => 'div',
+          '#value' => $moderation_value,
+        ],
+      ];
+      array_push($render_array, $line);
+    }
+
+    return $render_array;
 
   }
 
   /**
    * Get moderation state of most recent revision by nid.
+   *
+   * @param string $nid
+   *   The node id.
+   *
+   * @return array
+   *   An array of revision values.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  private function nidToModeration(string $nid) {
+  private function nidToModerationField(string $nid) {
+
+    // Get the id of the latest revision.
     $vid = \Drupal::entityTypeManager()
       ->getStorage('node')
       ->getLatestRevisionId($nid);
 
+    /** @var \Drupal\node\Entity\Node $revision */
     $revision = \Drupal::entityTypeManager()
       ->getStorage('node')
       ->loadRevision($vid);
 
-    $state = $revision->get('moderation_state')->getValue();
+    $state = $revision->moderation_state->getValue();
+    $reviewer = $revision->reviewer->getValue();
 
-    return isset($state[0]['value']) ? $state[0]['value'] : '';
+    if (isset($user[0]['target_id'])) {
+      $account = User::load($reviewer[0]['target_id']);
+      $username = $account->getUsername();
+    }
+
+    return [
+      'state' => isset($state[0]['value']) ? $state[0]['value'] : '',
+      'username' => $username ?? NULL,
+    ];
   }
 
 }
