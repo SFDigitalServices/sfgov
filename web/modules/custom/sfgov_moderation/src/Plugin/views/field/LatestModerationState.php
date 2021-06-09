9bbfe6be96dddadaec5f4675f4b5bd7133d11f72
zakiya <zakiya@chapterthree.com>
2021-06-08 19:00:57 -0700

    [SG-1143] Move latest revision logic to service.

--- web/modules/custom/sfgov_moderation/src/Plugin/views/field/LatestModerationState.php
+++ web/modules/custom/sfgov_moderation/src/Plugin/views/field/LatestModerationState.php
@@ -20,8 +20,13 @@ class LatestModerationState extends FieldPluginBase {
    */
   public function render(ResultRow $values) {
 
+    /** @var \Drupal\node\NodeInterface $node */
     $node = $this->getEntity($values);
-    $moderation_values = $this->nidToModerationField($node->id());
+
+    /** @var \Drupal\sfgov_moderation\ModerationUtilServiceInterface $moderationUtil */
+    $moderationUtil = \Drupal::service('sfgov_moderation.util');
+
+    $moderation_values = $moderationUtil->getModerationFields($node);
 
     $render_array = [];
     foreach ($moderation_values as $key => $moderation_value) {
@@ -37,59 +42,6 @@ class LatestModerationState extends FieldPluginBase {
 
     return $render_array;
 
-  }
-
-  /**
-   * Get moderation state of most recent revision by nid.
-   *
-   * @param string $nid
-   *   The node id.
-   *
-   * @return array
-   *   An array of revision values.
-   *
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
-   */
-  private function nidToModerationField(string $nid) {
-
-    // Get the id of the latest revision.
-    $vid = \Drupal::entityTypeManager()
-      ->getStorage('node')
-      ->getLatestRevisionId($nid);
-
-    /** @var \Drupal\node\Entity\Node $revision */
-    $revision = \Drupal::entityTypeManager()
-      ->getStorage('node')
-      ->loadRevision($vid);
-
-    // Get State.
-    $state = $revision->moderation_state->getValue();
-
-    // Get Reviewer.
-    $reviewer = $revision->reviewer->getValue();
-    if (isset($reviewer[0]['target_id'])) {
-      $account = User::load($reviewer[0]['target_id']);
-      $username = $account->getUsername();
-    }
-
-    // Get Department.
-    $bundle = $revision->bundle();
-    $dept_field_name = \Drupal::service('sfgov_moderation.util')->getDepartmentFieldName($bundle);
-    if ($revision->hasField($dept_field_name)) {
-      $department = $revision->get($dept_field_name)->getValue();
-
-      if(isset($department[0]['target_id'])) {
-        $node = Node::load($department[0]['target_id']);
-        $department_label = $node->label();
-      }
-    }
-
-    return [
-      'state' => isset($state[0]['value']) ? $state[0]['value'] : '',
-      'username' => $username ?? NULL,
-      'department' => $department_label ?? NULL,
-    ];
   }
 
 }
