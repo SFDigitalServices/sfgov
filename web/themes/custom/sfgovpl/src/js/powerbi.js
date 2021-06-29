6574c0615835df76b51e359b15e1befc7c52f8bd
arshad <arshad@chummun.com>
2021-06-29 14:26:11 +0400

    [sg-1412] Add kbd instructions to power bi embeds

--- web/themes/custom/sfgovpl/src/js/powerbi.js
+++ web/themes/custom/sfgovpl/src/js/powerbi.js
@@ -32,6 +32,24 @@
           }
         })
       }
+
+      // We cannot bind focus directly to the iframe.
+      // Unless we do polling (which is not efficient).
+      // See https://stackoverflow.com/questions/5456239/detecting-when-an-iframe-gets-or-loses-focus
+      // Instead we bind to the wrapper container and then shift focus to the iframe.
+      $charts.on('focus', function () {
+        // Add the focus class to the iframe to show visual guides.
+        $(this).find('> iframe')
+          .addClass('focus')
+          .focus();
+        $('.sfgov-powerbi-embed__kbd').removeClass('hidden');
+      })
+
+      // Handles tabbing away from iframe.
+      $(window).on('focus', function () {
+        $('iframe.focus').removeClass('focus');
+      })
+
     }
   };
 })(jQuery, Drupal);
