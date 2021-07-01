2127b817559a752696890481f76b3fc9c0d8ff1b
Anthony Kong <anthony.kong@sfgov.org>
2021-06-30 17:50:54 -0700

    SG-1324 - adjust heights for image containers

--- web/themes/custom/sfgovpl/src/js/campaign.js
+++ web/themes/custom/sfgovpl/src/js/campaign.js
@@ -5,14 +5,18 @@
       $('.campaign-facts').each(function() {
         var images = $(this).find('img');
         if(images.length > 0) {
-          var tallestImage = images[0];
-          for(var i=1; i<images.length; i++) {
-            if($(images[i]).height() > $(tallestImage).height()) {
-              tallestImage = images[i];
+          if(window.innerWidth > 768) {
+            var tallestImage = images[0];
+            for(var i=1; i<images.length; i++) {
+              if($(images[i]).height() > $(tallestImage).height()) {
+                tallestImage = images[i];
+              }
             }
+            var tallestHeight = $(tallestImage).height();
+            $(this).find('.fact-item .item-wrapper').height(tallestHeight); 
+          } else {
+            $(this).find('.fact-item .item-wrapper').css({'height':'auto'})
           }
-          var tallestHeight = $(tallestImage).height();
-          $(this).find('.fact-item .item-wrapper').height(tallestHeight);
         } else {
           $(this).find('.fact-item .image').hide();
         }
