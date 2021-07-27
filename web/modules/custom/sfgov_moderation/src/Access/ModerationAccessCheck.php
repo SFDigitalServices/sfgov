ed2e308abedbf4b2f4a9581d841cf08e5db47528
zakiya <zakiya@chapterthree.com>
2021-07-27 09:54:46 -0700

    [SG-1143] Do not allow users to translate content types unless allowed by permissions table.

--- web/modules/custom/sfgov_moderation/src/Access/ModerationAccessCheck.php
+++ web/modules/custom/sfgov_moderation/src/Access/ModerationAccessCheck.php
@@ -4,16 +4,57 @@ namespace Drupal\sfgov_moderation\Access;
 
 use Drupal\Core\Routing\Access\AccessInterface;
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 
-
+/**
+ * Custom access checking for SF.gov.
+ */
 class ModerationAccessCheck implements AccessInterface {
 
-  public function access(AccountInterface $account) {
+  /**
+   * The language manager service.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
 
-    // @todo Only target translation edit pages.
-    // @todo Use dynamic permissions like `translate [content type] node`
-    return (!$account->hasPermission('create translations')) ? AccessResult::allowed() : AccessResult::forbidden();
+  /**
+   * Constructor.
+   */
+  public function __construct(LanguageManagerInterface $language_manager) {
+    $this->languageManager = $language_manager;
+  }
+
+  /**
+   * Checks access.
+   *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The parametrized route.
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   The currently logged in account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access result.
+   */
+  public function access(RouteMatchInterface $route_match, AccountInterface $account) {
+
+    // Get values. Set defaults.
+    $language = $this->languageManager->getCurrentLanguage()->getId();
+    $node = $route_match->getParameter('node');
+    $bundle = $node->bundle();
+    $access = AccessResult::allowedIfHasPermission($account, sprintf('edit any %s content', $bundle));
+
+    // Deny access to users that don't have permission
+    // to edit a node's translation.
+    if ($language != 'en') {
+      $permission = sprintf('translate %s node', $bundle);
+      $access = $account->hasPermission($permission) ? AccessResult::allowed() : AccessResult::forbidden();
+
+    }
+    return $access;
+
   }
 
 }
