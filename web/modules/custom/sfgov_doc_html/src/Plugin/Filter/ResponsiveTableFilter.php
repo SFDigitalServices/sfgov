e02c1276ec5c3ed7a34a8c9447bbc891d7926b14
arshad <arshad@chummun.com>
2021-09-07 11:57:16 +0400

    [SG-1444] Implement responsive table filter

---
+++ web/modules/custom/sfgov_doc_html/src/Plugin/Filter/ResponsiveTableFilter.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\sfgov_doc_html\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Provides a filter to handle responsive tables.
+ *
+ * @Filter(
+ *   id = "responsive_table",
+ *   module = "sfgov_doc_html",
+ *   title = @Translation("Responsive tables"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE
+ * )
+ */
+class ResponsiveTableFilter extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    $document = Html::load($text);
+    $tables = $document->getElementsByTagName('table');
+    /** @var \DOMElement $table */
+    foreach ($tables as $index => $table) {
+      $table_index = $index + 1;
+      $table_id = "table-{$table_index}-caption";
+
+      $className = $table->getAttribute('class');
+      if (strpos($className, 'sfgov-table') === FALSE) {
+        $table->setAttribute('class', $className . ' sfgov-table sfgov-table-with-padding');
+      }
+
+      // Wrap the table.
+      if (strpos($table->parentNode->getAttribute('class'), 'sfgov-table') === FALSE) {
+        $table_wrapper = $document->createElement('div');
+        $table_wrapper->setAttribute('class', 'sfgov-table-wrapper');
+        $table_copy = $table->cloneNode(TRUE);
+        $table_wrapper->appendChild($table_copy);
+        $table->parentNode->replaceChild($table_wrapper, $table);
+
+        // Add table caption before responsive wrapper.
+        if ($captions = $table->getElementsByTagName('caption')) {
+          $table_caption = $document->createElement('p');
+          $table_caption->setAttribute('id', $table_id);
+          $table_caption->setAttribute('class', 'sfgov-table-caption');
+          $table_caption->nodeValue = $captions->item(0)->nodeValue;
+          $table_wrapper->parentNode->insertBefore($table_caption, $table_wrapper);
+          $table_copy->setAttribute('aria-labelledby', $table_id);
+
+          // Delete captions.
+          foreach ($table_copy->childNodes as $child) {
+            if ($child->nodeName === 'caption') {
+              $table_copy->removeChild($child);
+            }
+          }
+        }
+      }
+    }
+    return new FilterProcessResult(Html::serialize($document));
+  }
+
+}
