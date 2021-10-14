36ea9b38f52eaef3aa15040f71b5a56efaa42fbe
Anthony Kong <anthony.kong@sfgov.org>
2021-10-13 19:30:30 -0700

    create users for multidev testing

---
+++ web/private/scripts/drush-create-users/drush-create-users.php
@@ -0,0 +1,31 @@
+<?php
+  require dirname(__DIR__) . '/../shared.php';
+
+  // get the roles
+  ob_start();
+  passthru('drush role:list --fields=label --format=json');
+  $roles = ob_get_contents();
+  ob_end_clean();
+  $json = json_decode($roles);
+
+  $pw = _get_secrets(['drush_pw'])['drush_pw'];
+  $debug = '';
+
+  foreach ($json as $role => $val) {
+    if ($role !== 'authenticated' && $role !== 'anonymous') {
+      $machineNameRole = $role; 
+      $normalizedRole = strtolower(str_replace('_', '', $role));
+      $user = 'test.' . $normalizedRole;
+      $email = $user . '@test.com';
+      $exitStatus = null;
+      $output = null;
+      exec("drush user:create ${user} --mail=\"${email}\" --password=\"${pw}\"", $output, $exitStatus);
+      if ($exitStatus == 0) {
+        exec("drush user-add-role \"${machineNameRole}\" ${user}");
+        $debug .= "  - user ${user} created with role {$machineNameRole}\n";
+      }
+    }
+  }  
+
+  _test_hook_slack_notification("create users: \n" . (strlen($debug) > 0 ? $debug : '  no users created'));
+  
