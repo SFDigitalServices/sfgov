2efd450b9050f9794dfee8585e140b5e4968b87f
Anthony Kong <anthony.kong@sfgov.org>
2021-10-19 00:24:27 -0700

    enable performance settings for all envs

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -86,19 +86,20 @@ if (defined('PANTHEON_ENVIRONMENT')) {
     $settings['trusted_host_patterns'][] = '^sfgov.org$';
   }
 
-  switch($_ENV['PANTHEON_ENVIRONMENT']) {
-    // Responsibility of forms is handled by formbuilder
-    // https://github.com/SFDigitalServices/webform
-    // Comment out these lines to remove the dependency.
-    case 'live':
-    case 'test':
-      $settings['formbuilder_include_url'] = 'https://formbuilder-sf.herokuapp.com';
-      break;
-    case 'dev':
-    default:
-      $settings['formbuilder_include_url'] = 'https://formbuilder-sf-staging.herokuapp.com';
-
-  }
+  // enable performance settings for all envs
+  $conf['cache'] = 1;
+  // Block caching - enabled.
+  $conf['block_cache'] = 1;
+  // Expiration of cached pages - 15 minutes.
+  $conf['page_cache_maximum_age'] = 900;
+  // Aggregate and compress CSS files in Drupal - on.
+  $conf['preprocess_css'] = 1;
+  // Aggregate JavaScript files in Drupal - on.
+  $conf['preprocess_js'] = 1;
+  // minify HTML.
+  $conf['minify_html'] = 1;
+  // Use Minified JavaScript files.
+  $conf['minify_js'] = 1;
 }
 
 /**
