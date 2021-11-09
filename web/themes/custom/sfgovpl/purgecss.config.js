ff0ce07b9b41daf99b158463a8758c0d3bea97ae
Shawn Allen <shawnbot@users.noreply.github.com>
2021-11-09 15:04:13 -0800

    chore: safelist margin + padding CSS utilities

--- web/themes/custom/sfgovpl/purgecss.config.js
+++ web/themes/custom/sfgovpl/purgecss.config.js
@@ -4,5 +4,11 @@ module.exports = {
     'templates/**/*.{twig,html}',
     'src/**/*.js',
     '../../../modules/custom/**/*.{html,inc,js,php,theme,twig}'
-  ]
+  ],
+  safelist: {
+    patterns: [
+      // preserve all "basic" margin and padding utilities (for forms)
+      /[mp][trblxy]*-\d+/
+    ]
+  }
 }
