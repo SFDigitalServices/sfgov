bc5aee1b77a1b69831e138b18fb759dedcafe13d
Shawn Allen <shawnbot@users.noreply.github.com>
2021-11-09 16:36:42 -0800

    chore: also safelist bg-* and text-* color utilities

--- web/themes/custom/sfgovpl/purgecss.config.js
+++ web/themes/custom/sfgovpl/purgecss.config.js
@@ -8,7 +8,11 @@ module.exports = {
   safelist: {
     greedy: [
       // preserve all "basic" margin and padding utilities (for forms)
-      /\b[mp][trblxy]?-\d+/
+      /\b[mp][trblxy]?-\d+/,
+      // background color utilities
+      /bg-(black|white|slate|blue|green|red|purple|yellow|grey)/,
+      // text color utilities
+      /text-(black|white|slate|blue|green|red|purple|yellow|grey|secondary)/,
     ]
   }
 }
