a96a6ecf8a9673c80737afb0e6938f0ffaa7b65b
Anthony Kong <anthony.kong@sfgov.org>
2021-11-16 14:02:19 -0800

    get target ids of campaign resources

--- web/private/scripts/test/resources-migration.php
+++ web/private/scripts/test/resources-migration.php
@@ -4,6 +4,7 @@ require_once DRUPAL_ROOT . '/core/includes/bootstrap.inc';
 
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\node\Entity\Node;
+use Drupal\Core\Entity;
 
 $users = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties(['mail'=>'webmaster@sfgov.org']);
 $user = reset($users);
@@ -14,5 +15,19 @@ $nodes = Node::loadMultiple($nids);
 
 foreach($nodes as $node) {
   $title = $node->getTitle();
-  echo $node->gettype() . ": " . $title . "\n";
+  echo $node->gettype() . ": " . $title . " (". $node->id() . ")\n";
+  // check for field
+  if ($node->hasField('field_contents')) {
+    $fieldValues = $node->get('field_contents')->getValue();
+    if (!empty($fieldValues)) {
+      foreach ($fieldValues as $fieldValue) {
+        $targetId = $fieldValue['target_id'];
+        $paragraph = Paragraph::load($targetId);
+        $paragraphType = $paragraph->getType();
+        if ($paragraphType == 'campaign_resources') {
+          echo "  target_id:" . $targetId . ", paragraph type:" . $paragraphType . "\n";
+        }
+      }
+    }
+  }
 }
