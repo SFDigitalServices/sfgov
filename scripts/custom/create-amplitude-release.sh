a337ebf6236d696988e3a8f3ca74973904060931
Shawn Allen <shawnbot@users.noreply.github.com>
2021-11-29 10:27:14 -0800

    fix(amplitude): make Amplitude releases end, add more info

--- scripts/custom/create-amplitude-release.sh
+++ scripts/custom/create-amplitude-release.sh
@@ -7,19 +7,26 @@ elif [[ "$AMPLITUDE_AUTH" =~ (^:|:$) ]]; then
   echo "Skipping Amplitude release because AMPLITUDE_AUTH is malformed (missing either API or secret)"
   exit 0
 fi
-  
-# see: https://developers.amplitude.com/docs/releases-api
-release_start=$(date +"%Y-%m-%d %H:%M:00")
-# version tag?
-version="$release_start"
-title="Automated release from deployment at $release_start"
-# commit message?
-description=""
-# merge commit author?
-created_by=""
 
+# our base timestamp is up to the minute so that we can tweak the start and end
+timestamp=$(date +"%Y-%m-%d %H:%M")
+# releases without an end "stack" on top of one another in the Amplitude UI, so
+# we "end" our releases a second after they start
+release_start="$timestamp:00"
+release_end="$timestamp:01"
+# our version is just the timestamp rounded to the hour (YYYYMMDDHH), since it's
+# (currently) impossible to release more frequently
+version=$(date +"%Y%m%d%H")
+title=$(date)
+# commit message
+description="Automated release from CI. Git commit:\n\n$(git log -1)"
+# commit author
+created_by=$(git log -1 --format='%ae')
+
+# API docs: https://developers.amplitude.com/docs/releases-api
 curl -u "$AMPLITUDE_AUTH" -X POST \
   -F release_start="$release_start" \
+  -F release_end="$release_end" \
   -F version="$version" \
   -F title="$title" \
   -F description="$description" \
