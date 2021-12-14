7162017c0eb71b7b41b14b00bacf402eba8d50af
Anthony Kong <anthony.kong@sfgov.org>
2021-12-14 03:15:16 -0800

    get some scores

--- web/modules/custom/sfgov_readability/js/test.js
+++ web/modules/custom/sfgov_readability/js/test.js
@@ -1,16 +1,70 @@
-// import {parse} from 'https://unpkg.com/node-html-parser@5.1.0/dist/index.js'
 import {automatedReadability} from 'https://unpkg.com/automated-readability@2.0.0/index.js'
-import {split} from 'https://unpkg.com/sentence-splitter@3.2.2/lib/sentence-splitter.js'
+
+// score for https://sf.gov/information/masks-and-face-coverings-added-protection-coronavirus
+// based on hemingway app sentence, word, and character count
+const hemscore = automatedReadability({
+  sentence: 101,
+  word: 1063,
+  character: 6189
+})
+console.log(hemscore)
+
+const text = document.querySelector('main #block-sfgovpl-content').innerText
+
+// get sentences
+const sentences = text.split(/\.|\?|\!|\n/)
+let sentencesData = [];
+let sentenceCount = 0
+let totalWordCount = 0
+let totalCharCount = 0
+
+for (let i=0; i<sentences.length; i++) {
+  let sentence = sentences[i].trim()
+  // if(sentence.length > 0) {
+  //   let words = sentence.split(' ')
+  //   if(words.length == 1) { // single words don't count as full sentences?
+  //     wordCount += 1
+  //     charCount += words[0].length
+  //   } else {
+  //     sentenceCount++;
+  //     wordCount += words.length
+  //     charCount += sentence.length
+  //   }
+  // }
+
+  if(sentence.length > 0) {
+    let wordCount = sentence.split(' ').length
+    let charCount = sentence.length
+    
+    totalWordCount += wordCount
+    totalCharCount += charCount
+    sentenceCount++
+
+    let obj = {
+      sentence: sentence,
+      wordCount: wordCount,
+      charCount: charCount,
+      score: automatedReadability({
+        sentence: 1,
+        word: wordCount,
+        character: charCount
+      })
+    }
+
+    sentencesData.push(obj)
+  }
+}
+
+console.log('sentences: ' + sentenceCount)
+console.log('words: ' + totalWordCount)
+console.log('characters: ' + totalCharCount)
 
 const score = automatedReadability({
-  sentence: 6,
-  word: 151,
-  character: 623
+  sentence: sentenceCount,
+  word: totalWordCount,
+  character: totalCharCount
 })
 
-const html = document.querySelector('main #block-sfgovpl-content')[0].innerText
+console.log('score: ' + score)
 
-
-
-
-console.log(score)
+console.log(sentencesData)
