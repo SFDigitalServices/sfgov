3464d5d4cf372e3f17f09d94f3a7a88de8dd0ad6
Anthony Kong <anthony.kong@sfgov.org>
2022-01-03 10:28:15 -0800

    SG-1432 - add patches directory to purgecss config

--- web/themes/custom/sfgovpl/purgecss.config.js
+++ web/themes/custom/sfgovpl/purgecss.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     'templates/**/*.{twig,html}',
     'src/**/*.js',
-    '../../../modules/custom/**/*.{html,inc,js,php,theme,twig}'
+    '../../../modules/custom/**/*.{html,inc,js,php,theme,twig}',
+    '../../../../patches/**/*.patch'
   ],
   defaultExtractor: (content) => content.match(/[\w-:./]+(?<!:)/g) || [],
   safelist: {
@@ -14,7 +15,6 @@ module.exports = {
       /bg-(black|white|slate|blue|green|red|purple|yellow|grey)/,
       // text color utilities
       /text-(black|white|slate|blue|green|red|purple|yellow|grey|secondary)/,
-      /(hocus:)?(no-)?underline/,
     ]
   }
 }
