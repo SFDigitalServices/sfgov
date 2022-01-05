ac5214b58246f81e76ec4d72777e6358123c2480
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-04 16:23:02 -0800

    chore(lint): add eslint config to sfgovpl

---
+++ web/themes/custom/sfgovpl/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  plugins: ['sfgov'],
+  extends: [
+    'plugin:sfgov/recommended',
+    'plugin:sfgov/babel'
+  ],
+  rules: {
+    'comma-dangle': 'error',
+    'newline-per-chained-call': ['warn', { ignoreChainWithDepth: 3 }],
+    'no-magic-numbers': ['warn', { ignore: [0, 1] }],
+    'no-console': 'warn',
+    'semi': 'error',
+    // we don't need "use strict" directives: @babel/preset-env adds them automatically
+    strict: ['error', 'never']
+  }
+}
