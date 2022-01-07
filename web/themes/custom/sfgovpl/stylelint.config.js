8d6667f2700e31ea2ebb9a9d910807100e12d9b0
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-06 23:50:22 -0800

    chore(lint): demote more stylelint rules to warnings

--- web/themes/custom/sfgovpl/stylelint.config.js
+++ web/themes/custom/sfgovpl/stylelint.config.js
@@ -5,10 +5,17 @@ module.exports = {
     'stylelint-config-standard-scss'
   ],
   rules: {
-    'color-function-notation': null, // FIXME: 'modern'
     'alpha-value-notation': null, // FIXME: 'percentage'
+    'color-function-notation': null, // FIXME: 'modern'
+    'declaration-block-no-redundant-longhand-properties': [true, { severity: 'warning' }],
+    'function-url-quotes': ['always', { severity: 'warning' }],
     'no-descending-specificity': [true, { severity: 'warning' }],
+    'number-max-precision': [4, { severity: 'warning' }],
+    'scss/comment-no-empty': null,
     'scss/double-slash-comment-whitespace-inside': ['always', { severity: 'warning' }],
+    'scss/no-global-function-names': null,
+    'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['horizontal', 'vertical']}],
     'string-quotes': 'single'
   }
 }
