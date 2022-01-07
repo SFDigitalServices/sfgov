897fcc35f197602f711018c46d0b6fc82c7eb5fe
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-06 21:23:53 -0800

    chore(lint): switch back to single quotes for consistency w/JS

--- web/themes/custom/sfgovpl/prettier.config.js
+++ web/themes/custom/sfgovpl/prettier.config.js
@@ -2,5 +2,6 @@ module.exports = {
   // We have some files with CRLF line endings, which Prettier treats as a violation.
   // The fix is to run, say, dos2unix on those files, remove this setting, then re-run
   // Prettier and make sure that it doesn't flag them as invalid.
-  endOfLine: 'auto'
+  endOfLine: 'auto',
+  singleQuote: true
 }
