a2d0ba02715a532c1e85e194df9f8721a4fafee3
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-06 21:13:31 -0800

    chore(lint): remove rest of JS-related prettier options

--- web/themes/custom/sfgovpl/prettier.config.js
+++ web/themes/custom/sfgovpl/prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  endOfLine: 'auto',
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'none'
+  // We have some files with CRLF line endings, which Prettier treats as a violation.
+  // The fix is to run, say, dos2unix on those files, remove this setting, then re-run
+  // Prettier and make sure that it doesn't flag them as invalid.
+  endOfLine: 'auto'
 }
