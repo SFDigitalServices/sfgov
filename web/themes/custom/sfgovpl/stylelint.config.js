bdae4a2e02cca30c266f17efdeaec70091835b06
Shawn Allen <shawnbot@users.noreply.github.com>
2022-01-07 00:07:53 -0800

    chore(lint): keep building up stylelint config

--- web/themes/custom/sfgovpl/stylelint.config.js
+++ web/themes/custom/sfgovpl/stylelint.config.js
@@ -5,17 +5,35 @@ module.exports = {
     'stylelint-config-standard-scss'
   ],
   rules: {
-    'alpha-value-notation': null, // FIXME: 'percentage'
-    'color-function-notation': null, // FIXME: 'modern'
+    // FIXME: 'percentage'?
+    'alpha-value-notation': null,
+    // FIXME: 'modern'?
+    'color-function-notation': null,
+    // FIXME: promote this to an error once fixed
     'declaration-block-no-redundant-longhand-properties': [true, { severity: 'warning' }],
+    // FIXME: promote this to an error once fixed
     'function-url-quotes': ['always', { severity: 'warning' }],
+    // FIXME: promote this to an error once fixed
     'no-descending-specificity': [true, { severity: 'warning' }],
+    // FIXME: set this to a number that's significant and promote to error
     'number-max-precision': [4, { severity: 'warning' }],
+    // because we have periods in some of our partial filenames (before the final .scss)...
+    'scss/at-import-partial-extension': null,
+    // some comment blocks include empty "//" lines
     'scss/comment-no-empty': null,
-    'scss/double-slash-comment-whitespace-inside': ['always', { severity: 'warning' }],
+    // we don't care about variable names for now
+    'scss/dollar-variable-pattern': null,
+    // we don't care about newlines before inline comments
+    'scss/double-slash-comment-empty-line-before': null,
+    'scss/double-slash-comment-whitespace-inside': null,
+    // we're not using modern Sass (yet), so this is irrelevant
     'scss/no-global-function-names': null,
+    // we don't (yet) enforce class naming conventions
     'selector-class-pattern': null,
-    'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['horizontal', 'vertical']}],
+    // some "unknown" pseudo-classes are for visible scrollbars
+    'selector-pseudo-class-no-unknown': [true, {
+      ignorePseudoClasses: ['horizontal', 'vertical']
+    }],
     'string-quotes': 'single'
   }
 }
