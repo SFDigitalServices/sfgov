502638866091a88971e1f7ddfc3a8cade49157e7
rloos289 <rloos289@gmail.com>
2022-01-21 15:16:22 -0800

    1622: adjust display for meeting times

--- web/modules/custom/sfgov_dates/src/Plugin/Field/FieldFormatter/SfgovBlockDateFormatter.php
+++ web/modules/custom/sfgov_dates/src/Plugin/Field/FieldFormatter/SfgovBlockDateFormatter.php
@@ -5,25 +5,24 @@ namespace Drupal\sfgov_dates\Plugin\Field\FieldFormatter;
 use Drupal\sfgov_dates\Plugin\Field\FieldFormatter\SfgovDateFormatterBase;
 
 /**
- * Plugin implementation of the 'SfgovListDate' formatter.
+ * Plugin implementation of the 'SfgovBlockDate' formatter.
  *
  * @FieldFormatter(
- *   id = "sfgov_dates_list_date",
- *   label = @Translation("Sfgov List Date"),
+ *   id = "sfgov_dates_block_date",
+ *   label = @Translation("Sfgov Block Date"),
  *   field_types = {
  *     "smartdate",
  *     "daterange"
  *   }
  * )
  */
-class SfgovListDateFormatter extends SfgovDateFormatterBase {
+class SfgovBlockDateFormatter extends SfgovDateFormatterBase {
 
   public function setDateString($start_time, $end_time) {
-    return 'date' . $start_time;
+    return date('l, M j, Y, g:i a', $start_time);
   }
 
   public function setTimeString($start_time, $end_time) {
-    return 'time' . $start_time;
+    return FALSE;
   }
-
 }
