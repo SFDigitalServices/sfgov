6329078af417b4c74fc443517bd145b4e5d7b102
rloos289 <rloos289@gmail.com>
2022-01-28 14:51:23 -0800

    1634: more event theming

---
+++ web/modules/custom/sfgov_dates/src/Plugin/Field/FieldFormatter/SfgovTeaserDateFormatter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\sfgov_dates\Plugin\Field\FieldFormatter;
+
+use Drupal\sfgov_dates\Plugin\Field\FieldFormatter\SfgovDateFormatterBase;
+
+/**
+ * Plugin implementation of the 'SfgovTeaserDate' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "sfgov_dates_teaser_date",
+ *   label = @Translation("Sfgov Teaser Date"),
+ *   field_types = {
+ *     "smartdate",
+ *     "daterange"
+ *   }
+ * )
+ */
+class SfgovTeaserDateFormatter extends SfgovDateFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setDateString($start_time, $end_time) {
+    if ($this->dateRange) {
+      $date_string = date('D, M j', $start_time) . ' to ' . date('D, M j', $end_time);
+    }
+    else {
+      $date_string = date('l, F j', $start_time);
+    }
+    return $date_string;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setTimeString($start_time, $end_time) {
+    if ($this->timeRange) {
+      $time_string = date('g:i a', $start_time) . ' to ' . date('g:i a', $end_time);
+    }
+    else {
+      $time_string = date('g:i a', $start_time);
+    }
+    return $time_string;
+  }
+
+}
