096c4e82302eec744051afe4d37ed0cb4855b5d3
rloos289 <rloos289@gmail.com>
2022-02-04 09:49:16 -0800

    SG-1622: address comments

--- web/modules/custom/sfgov_dates/src/Plugin/Field/FieldWidget/SfgovSmartdateWidget.php
+++ web/modules/custom/sfgov_dates/src/Plugin/Field/FieldWidget/SfgovSmartdateWidget.php
@@ -25,7 +25,7 @@ class SfgovSmartdateWidget extends SmartDateDefaultWidget {
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $element = parent::formElement($items, $delta, $element, $form, $form_state);
     unset($element["duration"]["#title"]);
-    $element["duration"]["#title"] = t('End Time');
+    $element["duration"]["#title"] = $this->t('End Time');
     return $element;
     // Note, could do this with a widget alter hook if we don't have any other
     // reason for a custom widget.
