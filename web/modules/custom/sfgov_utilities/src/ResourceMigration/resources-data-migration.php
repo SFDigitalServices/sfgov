9ae280dc1acc46dbf68eff59c07fbd530191c61d
Anthony Kong <anthony.kong@sfgov.org>
2022-02-10 01:26:05 -0800

    SG-1527 - resource migration script and report

---
+++ web/modules/custom/sfgov_utilities/src/ResourceMigration/resources-data-migration.php
@@ -0,0 +1,25 @@
+<?php
+
+use Drupal\sfgov_utilities\ResourceMigration\ResourceMigration;
+
+$rm = new ResourceMigration();
+$rm->setDryRun(false);
+
+$rm->migrateAboutResources();
+$rm->migrateCampaignResources();
+$rm->migrateTopicsAndDepartments();
+$rm->migrateResourceCollections();
+
+$rm->verifyMigration();
+
+// echo "\/***** node report *****\/\n\n";
+// $rm->getNodeReport();
+// echo "\n\n";
+
+// echo "\/***** validation report *****\/\n\n";
+// $rm->getValidationReport();
+// echo "\n\n";
+
+// echo "\/***** duplicates report *****\/\n\n";
+// $rm->getDuplicateReport();
+// echo "\n\n";
