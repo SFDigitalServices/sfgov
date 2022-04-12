981161c550077a7436c521e2dcc3aa7dff4f8ef2
Gabe <gc3@3633830.no-reply.drupal.org>
2022-04-12 14:42:35 -0700

    SG-1708 - Alert styling

--- web/modules/custom/sfgov_utilities/js/sfgov_utilities_sandbox.js
+++ web/modules/custom/sfgov_utilities/js/sfgov_utilities_sandbox.js
@@ -1,9 +1,9 @@
 // Add sandbox alert
 const sandbox_alert = document.createElement("div");
 sandbox_alert.classList.add('sandbox-alert');
-sandbox_alert.innerHTML = `<p style="background: red; color: #fff; padding: 10px; 
+sandbox_alert.innerHTML = `<p style="background: #C9563A; color: #fff; padding: 10px; 
   margin: 0; font-size: 16px; font-weight: bold;">You are in the training version 
   of SF.gov. Your work will not be saved. To create live pages, go to 
-  <a href="https://sf.gov" style="color: #fff; text-decoration: underline;">SFGov</a></p>
+  <a href="https://sf.gov" style="color: #fff; text-decoration: underline;">SF.gov</a></p>
   `;
 document.body.prepend(sandbox_alert);
