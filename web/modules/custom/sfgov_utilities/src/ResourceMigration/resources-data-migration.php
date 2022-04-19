c6569d51e4ed4410d9f6fec8d36fe12f33a3c890
ant <anthony.kong@sfgov.org>
2022-04-19 13:34:54 -0700

    SG 1689 - SG-1644 migrate resources for department and topic resource sections (#1259)
    * SG-1644 - update topic to allow other_info_card (resource section)
    
    * SG-1644 - add deploy hook for topic and department resources with subheading migration
    
    * remove debug comment

--- web/modules/custom/sfgov_utilities/src/ResourceMigration/resources-data-migration.php
+++ web/modules/custom/sfgov_utilities/src/ResourceMigration/resources-data-migration.php
@@ -5,19 +5,25 @@ use Drupal\sfgov_utilities\ResourceMigration\ResourceMigration;
 $rm = new ResourceMigration();
 $rm->setDryRun(false);
 
-$rm->migrateAboutAndPublicBodyResources();
-$rm->migrateCampaignResources();
-$rm->migrateTopicsAndDepartments();
-$rm->migrateResourceCollections();
+// $rm->migrateAboutAndPublicBodyResources();
+// $rm->migrateCampaignResources();
+// $rm->migrateTopicsAndDepartments();
+// $rm->migrateResourceCollections();
 
-echo "\/***** verify migration report *****\/\n\n";
-$rm->verifyMigration();
-echo "\n\n";
+$rm->migrateTopicsAndDepartmentsResourceSubheading();
+
+// echo "\/***** verify migration report *****\/\n\n";
+// $rm->verifyMigration();
+// echo "\n\n";
 
 echo "\/***** node report *****\/\n\n";
 $rm->getNodeReport();
 echo "\n\n";
 
-echo "\/***** duplicates report *****\/\n\n";
-$rm->getDuplicateReport();
+echo "\/***** report *****\/\n\n";
+echo json_encode($rm->getReport(), JSON_UNESCAPED_SLASHES);
 echo "\n\n";
+
+// echo "\/***** duplicates report *****\/\n\n";
+// $rm->getDuplicateReport();
+// echo "\n\n";
