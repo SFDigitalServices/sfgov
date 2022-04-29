4c42886222f1ffeddaa0f939a4658f0478569965
anthony kong <anthony.kong@sfgov.org>
2022-04-29 01:57:09 -0700

    SG-1690 - add field_dept to data story

--- web/modules/custom/sfgov_utilities/sfgov_utilities.deploy.php
+++ web/modules/custom/sfgov_utilities/sfgov_utilities.deploy.php
@@ -4,6 +4,7 @@ use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\node\Entity\Node;
 use \Drupal\media\entity\Media;
 use Drupal\sfgov_utilities\ResourceMigration\ResourceMigration;
+use Drupal\sfgov_utilities\FieldDeptMigration\FieldDeptMigration;
 
 /**
  * Create media entities for existing profile field_photo_images and assign to new field_profile_photo media entity reference
@@ -305,42 +306,7 @@ function sfgov_utilities_deploy_05_dept_page_about() {
 }
 
 // migrate field_public_body references to field_dept
-function sfgov_utilities_deploy_06_info_page() {
-  $nids = \Drupal::entityQuery('node')->condition('type','information_page')->execute();
-  $nodes = Node::loadMultiple($nids);
-  
-  $report = [];
-  
-  foreach($nodes as $node) {
-    $nid = $node->id();
-    $fieldPublicBody = $node->get('field_public_body');
-    $fieldPublicBodyValues = $fieldPublicBody->getValue();
-    $fieldPublicBodyCount = count($fieldPublicBodyValues);
-    if($fieldPublicBodyCount > 0) {
-      $fieldDept = $node->get('field_dept');
-      for($i=0; $i<$fieldPublicBodyCount; $i++) {
-        $refId = $fieldPublicBodyValues[$i]['target_id'];
-        $oldRefNode = Node::load($refId);
-  
-        if(!empty($oldRefNode)) { // some older nodes may have empty references, ignore them
-          $report[] = [
-            'nid' => $nid,
-            'node_title' => $node->getTitle(),
-            'url' => 'https://sf.gov/node/' . $nid,
-            'field_public_body_ref' => $oldRefNode->getTitle(),
-            'field_public_body_ref_id' => $oldRefNode->id(),
-          ];
-    
-          // assign to new field
-          $fieldDept[] = [
-            'target_id' => $refId
-          ];
-        }
-  
-        // remove old ref
-        $fieldPublicBody->removeItem(0);
-      }
-    }
-    $node->save();
-  }
+function sfgov_utilities_deploy_06_field_dept_migration() {
+  $deptMigration = new FieldDeptMigration();
+  $deptMigration->migrateToFieldDept();
 }
