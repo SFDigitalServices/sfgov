1940cd23dd92202e35e38e6626b9f3f5f5f25055
anthony kong <anthony.kong@sfgov.org>
2022-04-29 20:07:51 -0700

    SG-1690 - update migration scripts

--- web/modules/custom/sfgov_utilities/src/Migration/FieldDeptMigration/field-dept-migration.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Drupal\sfgov_utilities\Migration\FieldDepartmentMigration\FieldDepartmentMigration;
-
-$deptsMigration = new FieldDepartmentMigration();
-$deptsMigration->migrateToFieldDepartments();
-
-echo json_encode($deptMigration->getReport(), JSON_UNESCAPED_SLASHES);
