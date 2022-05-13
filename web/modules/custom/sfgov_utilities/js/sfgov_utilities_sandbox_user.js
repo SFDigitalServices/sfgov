7cb757adb22bd7249f4b7db5d000258b07369664
anthony kong <anthony.kong@sfgov.org>
2022-05-13 16:14:41 -0700

    use design system utility classes

--- web/modules/custom/sfgov_utilities/js/sfgov_utilities_sandbox_user.js
+++ web/modules/custom/sfgov_utilities/js/sfgov_utilities_sandbox_user.js
@@ -4,12 +4,12 @@
   const selector = '.sfgov-user__content'
   const loginContainer = document.createElement('details')
 
-  loginContainer.style.width = '80%'
+  loginContainer.className = 'w-3/4'
   loginContainer.innerHTML = '<summary class="details__summary">Log in as a test user</summary>'
-  let html = '<ul class="details__content" style="margin: 0">'
+  let html = '<ul class="details__content m-0">'
   
   for (let user of sandboxUsers) {
-    html += '<li style="margin:0 0 10px 25px"><a data-user="' + user + '" href="">' + user + '</a></li>'
+    html += '<li class="mb-8 ml-28"><a data-user="' + user + '" href="">' + user + '</a></li>'
   }
 
   html += '</ul>'
