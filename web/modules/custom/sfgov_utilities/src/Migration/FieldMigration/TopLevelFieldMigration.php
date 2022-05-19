20acfd551b46723fc4fdf3bef94caf06690a46a8
ant <anthony.kong@sfgov.org>
2022-05-19 09:02:31 -0700

    SG-1690 - migrate content types to use field_departments (#1267)
    * SG-1690 - information page - add field_departments to information page content type
    
    * SG-1690 - information page - add migraton script for field_departments
    
    * SG-1690 - information page - update template and css for field_departments
    
    * SG-1690 - update migration scripts
    
    * SG-1690 - campaign - add field_departments to campaign
    
    * SG-1690 - campaign - update campaign templates to display field_departments
    
    * SG-1690 - migration - include campaign content type for migration
    
    * SG-1690 - department table - add field_departments
    
    * SG-1690 - event - add field_departments
    
    * SG-1690 - event - use the correctly spelled field name
    
    * SG-1690 - form confirmation page - add field_departments to form confirmation page
    
    * SG-1690 - form confirmation page - add form confirmation page for migration
    
    * SG-1690 - news - add field_departments to news content type
    
    * SG-1690 - news - update templates and css to use field_departments
    
    * SG-1690 - add news content type for field migration
    
    * SG-1690 - resource collection - add field_departments to resource collection
    
    * SG-1690 - resource collection - update template to use field_departments
    
    * SG-1690 - add resource collection for field migration
    
    * SG-1609 - campaign - update template to exclude field_departments for section content display
    
    * SG-1609 - include translations for field migration
    
    * SG-1690 - meeting - add field_departments to meeting content type
    
    * SG-1690 - meeting - add meeting content type for field migration, update template to use field_departments
    
    * SG-1690 - step by step - add and configure field_departments for step by step content type
    
    * SG-1690 - add step by step for field migration
    
    * SG-1690 - break up migration
    
    * SG-1690 - generalize top level field migration a little bit
    
    * SG-1690 - add some more reporting
    
    * SG-1690 - update field labels and reference types
    
    * SG-1690 - do not ignore empty refs
    
    * SG-1690 - update field_departments field label for information page
    
    * SG-1690 - reinstall group module temporarily to fix exception when deleting fields
    
    * SG-1690 - fix view references
    
    * SG-1690 - fix missing dept refs on campaign
    
    * SG-1690 - update label for meeting field_departments
    
    * unset vars to free up some memory

---
+++ web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/TopLevelFieldMigration.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\sfgov_utilities\Migration\FieldMigration;
+
+use Drupal\node\Entity\Node;
+use Drupal\sfgov_utilities\Utility;
+
+class TopLevelFieldMigration {
+
+  private $report;
+
+  public function __construct() {
+    $this->report = [];    
+  }
+  
+  public function getReport() {
+    return $this->report;
+  }
+
+  /*
+  * migrates values from one field to another field on the same content type
+  */
+  public function migrate($nodes, $fromFieldName, $toFieldName) {
+    foreach($nodes as $node) {
+      if(!$node->hasField($fromFieldName)) {
+        throw new \Exception("Field `$fromFieldName` does not exist on node of type `" . $node->getType() . "` with id " . $node->id());
+      }
+
+      $nid = $node->id();
+      $contentType = $node->getType();
+
+      $fromField = $node->get($fromFieldName);
+      $fromFieldValues = $fromField->getValue();
+      $toField = $node->get($toFieldName);
+      
+      if(!empty($fromFieldValues)) {
+        foreach($fromFieldValues as $fromFieldValue) {
+          $refId = $fromFieldValue['target_id'];
+          $refNode = Node::load($refId);
+          $refNodeTitle = $refNode ? $refNode->getTitle() : 'empty reference, no title';
+          
+          $reportLang = $node->get('langcode')->value != 'en' ? ($node->get('langcode')->value . '/') : '';
+          $this->report[] = [
+            'nid' => $nid,
+            'content_type' => $node->getType(),
+            'language' => $node->get('langcode')->value,
+            'node_title' => $node->getTitle(),
+            'url' => 'https://sf.gov/'. $reportLang . 'node/' . $nid,
+            'status' => $node->isPublished(),
+            'field_from' => $fromFieldName,
+            'field_to' => $toFieldName,
+            'ref_node_title' => $refNodeTitle,
+            'ref_id' => $refId,
+          ];
+
+          echo "updating (" . $node->getType() . ") [$nid] " . $node->getTitle() . "\n";
+
+          // assign to new field
+          $toField[] = [
+            'target_id' => $refId
+          ];
+
+          // remove old ref
+          $fromField->removeItem(0);
+
+          unset($refNode);
+        }
+        $node->save();
+      }
+    }
+  }
+}
