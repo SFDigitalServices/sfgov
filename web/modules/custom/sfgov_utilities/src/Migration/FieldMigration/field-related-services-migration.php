f9ed9e7c24a445424018ca809e28190adf3a4c6e
anthony kong <anthony.kong@sfgov.org>
2022-06-06 15:32:42 -0700

    SG-1694 - output report on standalone script

--- web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/field-related-services-migration.php
+++ web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/field-related-services-migration.php
@@ -7,6 +7,8 @@ try {
   $transactionNodes = Utility::getNodes('transaction');
   $relatedServicesFieldMigration = new TopLevelFieldMigration();
   $relatedServicesFieldMigration->migrate($transactionNodes, 'field_transactions', 'field_related_content');
+
+  echo json_encode($relatedServicesFieldMigration->getReport(), JSON_UNESCAPED_SLASHES) . "\n";
 } catch(\Exception $e) {
   error_log($e->getMessage(), "\n");
 }
