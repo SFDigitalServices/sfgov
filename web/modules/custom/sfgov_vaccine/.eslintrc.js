f851a9e6dc3bed9808f2ca17b88b881e0c2208f1
Shawn Allen <shawnbot@users.noreply.github.com>
2022-06-15 19:22:35 -0700

    chore(eslint): be more lenient in sfgov_vaccine js

--- web/modules/custom/sfgov_vaccine/.eslintrc.js
+++ web/modules/custom/sfgov_vaccine/.eslintrc.js
@@ -1,5 +1,16 @@
 module.exports = {
   extends: [
     require.resolve('../../../../.eslintrc')
-  ]
+  ],
+  env: {
+    jquery: true
+  },
+  globals: {
+    google: 'readonly'
+  },
+  rules: {
+    eqeqeq: 'warn',
+    'no-magic-numbers': 0,
+    'no-unused-vars': 'warn'
+  }
 }
