6667df438ab2199b976406194937ff2aeb29ee25
Shawn Allen <shawnbot@users.noreply.github.com>
2022-06-16 11:14:05 -0700

    chore(vaccine-sites): add data-site attribute w/full JSON

--- web/modules/custom/sfgov_vaccine/src/Controller/VaccineController.php
+++ web/modules/custom/sfgov_vaccine/src/Controller/VaccineController.php
@@ -328,7 +328,7 @@ class VaccineController extends ControllerBase {
           'data-remote-asl' => $language_text['remote_asl'],
           'data-lat' => $location['lat'],
           'data-lng' => $location['lng'],
-          // 'data-site' => json_encode($site_data),
+          'data-site' => json_encode($site_data),
         ]),
         'last_updated' => date("F j, Y, g:i a", strtotime($last_updated)),
         'restrictions_text' => $restrictions_text,
