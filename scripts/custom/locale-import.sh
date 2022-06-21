9b8d563b70c064acd0e34fdceba3d054191fb25e
Shawn Allen <shawnbot@users.noreply.github.com>
2022-06-21 14:58:00 -0700

    feat(translations): add scripts/custom/locale-import.sh

---
+++ scripts/custom/locale-import.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+files=$(ls -1 config/translations/**.*.po || echo '')
+target_env="${TERMINUS_SITE?'TERMINUS_SITE is not set'}.${TERMINUS_ENV?'TERMINUS_ENV is not set'}"
+
+for file in $files; do
+  # remove everything up to and including the first ".", so
+  # "config/translations/custom.es.po" yields "es.po"
+  ext="${file##*.}"
+  # remove the .po suffix; what's left should be the language code
+  lang="${ext/.po/}"
+  terminus -n drush "$target_env" -- locale:import --type=customized --override=none "$lang" "$file"
+done
