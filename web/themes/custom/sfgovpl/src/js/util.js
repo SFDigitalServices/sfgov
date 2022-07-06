0cae16860c7a7712b0761008c4a2d7ad98960cc6
Shawn Allen <shawnbot@users.noreply.github.com>
2022-07-05 23:11:35 -0700

    chore(theme): set window.SFGOV.util explicitly

--- web/themes/custom/sfgovpl/src/js/util.js
+++ web/themes/custom/sfgovpl/src/js/util.js
@@ -1,16 +1,16 @@
-const SFGOV = {}
-
-SFGOV.util = {
-  getParam (name) {
-    let result = null
-    let tmp = []
-    location.search
-      .substr(1)
-      .split('&')
-      .forEach(item => {
-        tmp = item.split('=')
-        if (tmp[0] === name) result = decodeURIComponent(tmp[1])
-      })
-    return result
+window.SFGOV = {
+  util: {
+    getParam (name) {
+      let result = null
+      let tmp = []
+      location.search
+        .substring(1)
+        .split('&')
+        .forEach(item => {
+          tmp = item.split('=')
+          if (tmp[0] === name) result = decodeURIComponent(tmp[1])
+        })
+      return result
+    }
   }
 }
