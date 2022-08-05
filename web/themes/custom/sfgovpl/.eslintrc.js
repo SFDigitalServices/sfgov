28288752bedf9951c3accdf2d7e817400aa449c8
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-04 17:34:42 -0700

    chore(eslint): ignore dist/

--- web/themes/custom/sfgovpl/.eslintrc.js
+++ web/themes/custom/sfgovpl/.eslintrc.js
@@ -1,4 +1,7 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: '../../../../.eslintrc.js'
+  extends: '../../../../.eslintrc.js',
+  ignorePatterns: [
+    'dist/**'
+  ]
 }
