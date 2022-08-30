d9e0baca21b1650b35d0526d06cdee468c5d1199
anthony kong <anthony.kong@sfgov.org>
2022-08-30 14:24:50 -0700

    cleanup

--- web/private/scripts/drush-deploy/drush-deploy-hook.php
+++ web/private/scripts/drush-deploy/drush-deploy-hook.php
@@ -8,11 +8,11 @@ $output = [];
 $debug = "";
 
 echo "Running deploy:hook\n";
-// exec('drush deploy:hook -y', $output, $exitStatus);
 exec("drush deploy:hook -y", $output, $exitStatus);
+echo "Deploy hooks complete\n";
+
 foreach($output as $item) {
   $debug .= $item . "\n";
 }
-echo "Deploy hooks complete\n";
 
 _test_hook_slack_notification("deploy:hook executed with status $exitStatus:\n$debug");
