3a06195e7dd658ec9134f30efa8a355f9b5cb0ef
anthony kong <anthony.kong@sfgov.org>
2022-08-30 13:48:09 -0700

    send deploy hook output to slack notification

--- web/private/scripts/drush-deploy/drush-deploy-hook.php
+++ web/private/scripts/drush-deploy/drush-deploy-hook.php
@@ -3,8 +3,16 @@
 require dirname(__DIR__) . '/../shared.php';
 
 // Deploy hooks
+$exitStatus = null;
+$output = [];
+$debug = "";
+
 echo "Running deploy:hook\n";
-passthru('drush deploy:hook -y');
+// exec('drush deploy:hook -y', $output, $exitStatus);
+exec("drush deploy:hook -y", $output, $exitStatus);
+foreach($output as $item) {
+  $debug .= $item . "\n";
+}
 echo "Deploy hooks complete\n";
 
-_test_hook_slack_notification("deploy:hook executed");
+_test_hook_slack_notification("deploy:hook executed with status $exitStatus:\n$debug");
