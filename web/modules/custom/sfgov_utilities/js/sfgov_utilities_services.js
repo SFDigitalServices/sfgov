59bff59a0959515dc37039ffe1aeb0046bd9ce76
ant <anthony.kong@sfgov.org>
2022-09-19 18:22:07 -0700

    SG-1819 duplicate icons (#1402)
    * SG-1819 - update design system to 2.5
    
    * SG-1819 - fix js console error, explicitly set text-action
    
    * SG-1854 - fix copy dependencies for design system
    
    * SG-1819 - bring in design system icons.css

--- web/modules/custom/sfgov_utilities/js/sfgov_utilities_services.js
+++ web/modules/custom/sfgov_utilities/js/sfgov_utilities_services.js
@@ -1,4 +1,4 @@
-jQuery(function () {
+jQuery(function ($) {
   $(document).ready(function() {
     // Track if accordions have been activated
     var accordionActive = false;
@@ -151,4 +151,4 @@ jQuery(function () {
       }
     });
   });
-});
+}(jQuery));
