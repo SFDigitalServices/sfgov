a7da809a0194c7818995f2eb2426cb5ee66ec783
anthony kong <anthony.kong@sfgov.org>
2022-10-11 09:09:33 -0700

    SG-1854 - iterate public body urls to migrate

--- web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/field-dept-divisions-public-bodies.php
+++ web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/field-dept-divisions-public-bodies.php
@@ -13,6 +13,7 @@ try {
       $divisions = $dept->get('field_divisions')->getValue();
       $publicBodies = $dept->get('field_public_bodies')->getValue();
 
+      // migrate divisions
       if (!empty($divisions)) {
         // create agency section paragraph with divisions values
         $agencySection = Paragraph::create([
@@ -30,14 +31,33 @@ try {
         $dept->set('field_divisions', []);
       }
 
+      // migrate public bodies
       if (!empty($publicBodies)) {
         // check if url is internal or external
         // if external, it cannot be added as a related agencies item
+        
+        // iterate through public body links
+        foreach ($publicBodies as $publicBody) {
+          $link = Paragraph::load($publicBody['target_id']);
+          $uri = $link->get('field_link')->getValue()[0]['uri'];
+          
+          // error_log($dept->getTitle() . " (" . $dept->id() . "): " . $uri);
+          // error_log(\Drupal\Component\Utility\UrlHelper::isExternal($uri));
+          // error_log(\Drupal::service('path_alias.manager')->getAliasByPath($uri));
+
+          if (strpos($uri, 'entity')) { // internal url
+            
+          } elseif (strpos($uri, 'https://sf.gov')) { // absolute sf.gov url
+
+          } else { // other urls, report
+
+          }
+        }
       }
 
       $dept->save();
     }
   }
 } catch (\Exception $e) {
-  error_log($e->getMessage(), "\n");
+  error_log($e->getMessage());
 }
