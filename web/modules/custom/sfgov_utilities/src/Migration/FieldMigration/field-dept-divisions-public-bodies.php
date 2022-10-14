6f5a5140a4ea783f806bfeaea4f5b24ea6c42b24
anthony kong <anthony.kong@sfgov.org>
2022-10-14 10:15:26 -0700

    SG-1854 - add some debug for standalone migration script

--- web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/field-dept-divisions-public-bodies.php
+++ web/modules/custom/sfgov_utilities/src/Migration/FieldMigration/field-dept-divisions-public-bodies.php
@@ -8,70 +8,74 @@ use Drupal\sfgov_utilities\Utility;
 try {
   $deptNodes = Utility::getNodes('department');
   $externalUrls = [];
-  
+
   foreach($deptNodes as $dept) {
-    // migrate divisions
-    $divisions = $dept->get('field_divisions')->getValue();
-    $publicBodies = $dept->get('field_public_bodies')->getValue();
+    if ($dept->id() == 6823) {
+      // migrate divisions
+      $divisions = $dept->get('field_divisions')->getValue();
+      $publicBodies = $dept->get('field_public_bodies')->getValue();
 
-    // migrate divisions
-    if (!empty($divisions)) {
-      // create agency section paragraph with divisions values
-      $agencySection = Paragraph::create([
-        "type" => "agency_section",
-        "field_section_title_list" => "Divisions",
-        "field_nodes" => $divisions,
-      ]);
+      // migrate divisions
+      if (!empty($divisions)) {
+        // create agency section paragraph with divisions values
+        $agencySection = Paragraph::create([
+          "type" => "agency_section",
+          "field_section_title_list" => "Divisions",
+          "field_nodes" => $divisions,
+        ]);
 
-      // append new agency section to divisions and subcommittees field (field_paragraphs)
-      $agencySections = $dept->get('field_agency_sections')->getValue();
-      $agencySections[] = $agencySection;
-      $dept->set('field_agency_sections', $agencySections);
+        // append new agency section to divisions and subcommittees field (field_paragraphs)
+        $agencySections = $dept->get('field_agency_sections')->getValue();
+        $agencySections[] = $agencySection;
+        $dept->set('field_agency_sections', $agencySections);
 
-      // remove old field values
-      $dept->set('field_divisions', []);
-    }
-
-    // migrate public bodies
-    if (!empty($publicBodies)) {
-      $relatedAgencies = [];
-      // check if url is internal or external
-      // if external, it cannot be added as a related agencies item
-
-      // iterate through public body links
-      foreach ($publicBodies as $publicBody) {
-        $link = Paragraph::load($publicBody['target_id']);
-        $linkValue = $link->get('field_link')->getValue();
-        $uri = $linkValue[0]['uri'];
-        $text = $linkValue[0]['title'];
-        $drupalPath = "";
-
-        if (strpos($uri, 'entity') !== false || strpos($uri, 'https://sf.gov') !== false) { // internal url
-          $drupalPath = \Drupal::service('path_alias.manager')->getPathByAlias(parse_url($uri, PHP_URL_PATH));
-          $refNid = substr($drupalPath, strrpos($drupalPath, '/') + 1);
-          if (!empty($refNid)) {
-            $relatedAgencies[] = [
-              "target_id" => $refNid
-            ];
-          }
-        } else { // other urls, report
-          $externalUrls[] = [
-            "nid" => $dept->id(),
-            "public_body_url_text" => $text,
-            "public_body_url" => $uri,
-          ];
-        }
+        // remove old field values
+        $dept->set('field_divisions', []);
       }
 
-      echo "related agencies\n";
-      print_r($relatedAgencies);
-      echo "\n\n";
+      // migrate public bodies
+      if (!empty($publicBodies)) {
+        $relatedAgencies = [];
+        // check if url is internal or external
+        // if external, it cannot be added as a related agencies item
 
-      $dept->set('field_departments', $relatedAgencies);
-      $dept->set('field_public_bodies', []);
+        // iterate through public body links
+        foreach ($publicBodies as $publicBody) {
+          $link = Paragraph::load($publicBody['target_id']);
+          $linkValue = $link->get('field_link')->getValue();
+          $uri = $linkValue[0]['uri'];
+          $text = $linkValue[0]['title'];
+          $drupalPath = "";
+
+          if (strpos($uri, 'entity') !== false || strpos($uri, 'https://sf.gov') !== false) { // internal url
+            $drupalPath = \Drupal::service('path_alias.manager')->getPathByAlias(parse_url($uri, PHP_URL_PATH));
+            $refNid = substr($drupalPath, strrpos($drupalPath, '/') + 1);
+            echo "$drupalPath, ($refNid)\n";
+            if (!empty($refNid)) {
+              $relatedAgencies[] = [
+                "target_id" => $refNid
+              ];
+            }
+          } else { // other urls, report
+            $externalUrls[] = [
+              "nid" => $dept->id(),
+              "public_body_url_text" => $text,
+              "public_body_url" => $uri,
+            ];
+          }
+        }
+
+        echo "related agencies\n";
+        print_r($relatedAgencies);
+        echo "\n\n";
+
+        $dept->set('field_departments', $relatedAgencies);
+        $dept->set('field_public_bodies', []);
+      }
+
+      $dept->save();
     }
 
-    $dept->save();
   }
 
   echo "external urls\n";
