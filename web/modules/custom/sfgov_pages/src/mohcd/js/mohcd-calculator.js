ff15dce541a7e48a84f1c204309f3d8d0c267db4
Philip Boden <philip.boden@gmail.com>
2023-03-03 02:32:12 -0800

    javascript linting and cleanup

--- web/modules/custom/sfgov_pages/src/mohcd/js/mohcd-calculator.js
+++ web/modules/custom/sfgov_pages/src/mohcd/js/mohcd-calculator.js
@@ -1,20 +1,17 @@
-function fcalc() {
+function fcalc () {
   // this value is coming from configuration in sfgov_pages module
-  let fCurrentYearAMI = drupalSettings.sfgov.mohcd.calculator.currentYearAMI
-  let fBMRValuation
-  let fPurchaseYearSelect = document.getElementById("purchaseYear")
-  let fPurchasePrice = document.getElementById("purchasePrice").value
-  let fPurchaseYearAMI = parseFloat(fPurchaseYearSelect.value)
+  const fCurrentYearAMI = drupalSettings.sfgov.mohcd.calculator.currentYearAMI
+  const fPurchaseYearSelect = document.getElementById('purchaseYear')
+  let fPurchasePrice = document.getElementById('purchasePrice').value
+  const fPurchaseYearAMI = parseFloat(fPurchaseYearSelect.value)
   let fPurchaseYear = parseInt(fPurchaseYearSelect.options[fPurchaseYearSelect.selectedIndex].text)
-  let fFieldError = new Boolean()
-  let fFieldErrorArray = new Array()
-
-  //Clear previous error messages
+  let fFieldError
+  const fFieldErrorArray = []
+  // Clear previous error messages
   document.getElementById('purchasePriceError').setAttribute('style', 'display: none !important')
   document.getElementById('purchaseYearError').setAttribute('style', 'display: none !important')
   document.getElementById('valuationError').setAttribute('style', 'display: none !important')
-
-  //Check that the value of purchase price textbox is a number and if so, convert it to float
+  // Check that the value of purchase price textbox is a number and if so, convert it to float
   if ((isNaN(fPurchasePrice)) || (fPurchasePrice === '') || (fPurchasePrice = null)) {
     document.getElementById('purchasePriceError').setAttribute('style', 'display: inline !important')
     fPurchasePrice = 0
@@ -23,8 +20,7 @@ function fcalc() {
     fPurchasePrice = parseFloat(document.getElementById('purchasePrice').value)
     fFieldErrorArray[0] = 0
   }
-
-  //Check that Purchase Year is selected
+  // Check that Purchase Year is selected
   if (isNaN(fPurchaseYear)) {
     document.getElementById('purchaseYearError').setAttribute('style', 'display: inline !important')
     fPurchaseYear = 0
@@ -33,32 +29,29 @@ function fcalc() {
     fPurchaseYear = parseInt(document.getElementById('purchaseYear').value)
     fFieldErrorArray[1] = 0
   }
-
-  //Calculate BMR Valuation
-  fBMRValuation = (fPurchasePrice + (fPurchasePrice * ((fCurrentYearAMI - fPurchaseYearAMI) / fPurchaseYearAMI)))
-
-  //Check each field for an error, and if there is one, set fFieldError to true
+  // Calculate BMR Valuation
+  const fBMRValuation = (fPurchasePrice + (fPurchasePrice * ((fCurrentYearAMI - fPurchaseYearAMI) / fPurchaseYearAMI)))
+  // Check each field for an error, and if there is one, set fFieldError to true
   fFieldError = 0
   for (let a = 0; a < fFieldErrorArray.length; a++) {
     if (fFieldErrorArray[a] === 1) {
       fFieldError = 1
     }
   }
-
-  //Show the result in the "Current BMR Valuation" textbox
+  // Show the result in the "Current BMR Valuation" textbox
   if ((isNaN(fBMRValuation)) || (fFieldError === 1)) {
-    //Show error message if any fields have invalid values
-    document.getElementById("bmrValuation").setAttribute("style", "display: inline !important")
-    document.getElementById("bmrValuation").value = "n/a"
-    document.getElementById("valuationError").setAttribute("style", "display: inline !important")
+    // Show error message if any fields have invalid values
+    document.getElementById('bmrValuation').setAttribute('style', 'display: inline !important')
+    document.getElementById('bmrValuation').value = 'n/a'
+    document.getElementById('valuationError').setAttribute('style', 'display: inline !important')
   } else {
-    //Show BMR Valuation results (rounded and no decimals)
-    document.getElementById("bmrValuation").setAttribute("style", "display: inline !important")
-    document.getElementById("bmrValuation").value = Math.round(fBMRValuation)
+    // Show BMR Valuation results (rounded and no decimals)
+    document.getElementById('bmrValuation').setAttribute('style', 'display: inline !important')
+    document.getElementById('bmrValuation').value = Math.round(fBMRValuation)
   }
 }
 
-document.getElementById("btnCalc").addEventListener("click", (event) => {
+document.getElementById('btnCalc').addEventListener('click', (event) => {
   event.preventDefault()
   fcalc()
 })
