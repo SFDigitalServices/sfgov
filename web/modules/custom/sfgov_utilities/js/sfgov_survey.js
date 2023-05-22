e3fdac75dda5506135380abd1a68300a4d7e9777
anthony kong <anthony.kong@sfgov.org>
2023-05-22 11:47:32 -0700

    check if element with button class exists

--- web/modules/custom/sfgov_utilities/js/sfgov_survey.js
+++ web/modules/custom/sfgov_utilities/js/sfgov_survey.js
@@ -1,9 +1,11 @@
 (() => {
   const surveyBtn = document.querySelector('.survey-btn');
-  surveyBtn.addEventListener('click', (e) => {
-    e.stopPropagation();
-    e.preventDefault();
-    // doing it like this only allows the survey to show up once.  it's probably cookied or something
-    (function(t,e,s,o){var n,c,l;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],l=e.createElement(s),l.type="text/javascript",l.async=!0,l.id=o,l.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd3fGeVIeoaJmQrI93lw65EJe1FBHzsXXKMBGUYuf7LFm.js",c.parentNode.insertBefore(l,c))})(window,document,"script","smcx-sdk");
-  })
+  if (surveyBtn) {
+    surveyBtn.addEventListener('click', (e) => {
+      e.stopPropagation();
+      e.preventDefault();
+      // doing it like this only allows the survey to show up once.  it's probably cookied or something
+      (function(t,e,s,o){var n,c,l;t.SMCX=t.SMCX||[],e.getElementById(o)||(n=e.getElementsByTagName(s),c=n[n.length-1],l=e.createElement(s),l.type="text/javascript",l.async=!0,l.id=o,l.src="https://widget.surveymonkey.com/collect/website/js/tRaiETqnLgj758hTBazgd3fGeVIeoaJmQrI93lw65EJe1FBHzsXXKMBGUYuf7LFm.js",c.parentNode.insertBefore(l,c))})(window,document,"script","smcx-sdk");
+    })
+  }
 })();
