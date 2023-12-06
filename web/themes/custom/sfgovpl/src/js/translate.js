486291deb7cfb3c2d45dfee07d76a9c52d1a64e2
Daltyn06 <daltontyndall@gmail.com>
2023-12-06 15:05:49 -0500

    feat: dont autotranslate title metadata (#1625)
    * feat: dont autotranslate title metadata
    
    * feat: adding logic to gtranslate js
    
    ---------
    
    Co-authored-by: daltontyndall <dtyndall@symmetrimarketing.com>

--- web/themes/custom/sfgovpl/src/js/translate.js
+++ web/themes/custom/sfgovpl/src/js/translate.js
@@ -138,6 +138,7 @@ function SFGovTranslate () {
       // header and footer to be translated.
       if (drupalTranslation && drupalTranslation.status) {
         $('main[role="main"]').addClass('notranslate').attr('translate', 'no')
+        $('.google-title-translate').addClass('notranslate').attr('translate', 'no')
       }
       that.sfgovDoGTranslate('en|' + currentDrupalLanguage)
       that.addElementTranslationClass(currentDrupalLanguage)
