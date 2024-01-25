66c92c3b037598b3f59895807deb0edc4b49a457
rloos289 <rloos289@gmail.com>
2024-01-25 15:57:37 -0800

    get eck entities into the api

--- web/modules/custom/sfgov_api/src/Plugin/SfgApi/Eck/Address.php
+++ web/modules/custom/sfgov_api/src/Plugin/SfgApi/Eck/Address.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Drupal\sfgov_api\Plugin\SfgApi\Media;
+namespace Drupal\sfgov_api\Plugin\SfgApi\Eck;
 
 /**
  * Plugin implementation of the sfgov_api.
  *
  * @SfgApi(
- *   id = "locatione",
+ *   id = "eck_location",
  *   title = @Translation("ECK Location"),
  *   bundle = "location",
  *   wag_bundle = "address",
@@ -14,14 +14,24 @@ namespace Drupal\sfgov_api\Plugin\SfgApi\Media;
  *   langcode = {},
  * )
  */
-class Address extends SfgApiMediaBase {
+class Address extends SfgApiEckBase {
+
+  /**
+   * {@inheritDoc}
+   *
+   * Setting entity type here because ECK doesn't have a base entity type.
+   */
+  protected $entityType = 'location';
 
   /**
    * {@inheritDoc}
    */
   public function setCustomData($entity) {
-
-    $custom_data = [];
+    $custom_data = [
+      'address' => $entity->get('field_address')->getValue(),
+      'operating_hours' => $entity->get('field_operating_hours')->getValue(),
+      'text' => $entity->get('field_text')->getValue(),
+    ];
     return $custom_data;
   }
 
