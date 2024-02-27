fddb37d7432ad9626bdd723de6d1d8b143049e80
rloos289 <rloos289@gmail.com>
2024-02-27 14:54:54 -0800

    fetch data for reports

---
+++ web/modules/custom/sfgov_api/src/Plugin/SfgApi/Node/Report.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\sfgov_api\Plugin\SfgApi\Node;
+
+use Drupal\sfgov_api\Plugin\SfgApi\ApiFieldHelperTrait;
+
+/**
+ * Plugin implementation of the sfgov_api.
+ *
+ * @SfgApi(
+ *   id = "node_report",
+ *   title = @Translation("Node report"),
+ *   bundle = "report",
+ *   wag_bundle = "report",
+ *   entity_id = {},
+ *   langcode = {},
+ *   referenced_plugins = {
+ *     "node_department",
+ *     "media_file",
+ *     "paragraph_spotlight",
+ *  },
+ * )
+ */
+class Report extends SfgApiNodeBase {
+
+  use ApiFieldHelperTrait;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function setCustomData($entity) {
+    return [
+      'body' => $entity->get('body')->value,
+      'field_date_only' => $entity->get('field_date_only')->value,
+      'field_departments' => $this->getReferencedEntity($entity->get('field_departments')->referencedEntities()),
+      'field_description' => $entity->get('field_description')->value,
+      'field_print_version' => $this->getReferencedEntity($entity->get('field_print_version')->referencedEntities()),
+      'field_spotlight' => $this->getReferencedData($entity->get('field_spotlight')->referencedEntities()),
+    ];
+  }
+
+}
