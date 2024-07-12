8249aad1387b398563a5ba31a7f5497a95bed5f5
Shawn Allen <shawnbot@users.noreply.github.com>
2024-07-12 10:02:43 -0700

    chore(build): remove console calls from output in production

--- web/themes/custom/sfgovpl/babel.config.js
+++ web/themes/custom/sfgovpl/babel.config.js
@@ -1,4 +1,12 @@
+const PROD = process.env.NODE_ENV === 'production'
+const plugins = PROD
+  ? [
+      ['transform-remove-console', { exclude: ['warn', 'error'] }]
+    ]
+  : []
+
 module.exports = {
   extends: '../../../../babel.config.js',
-  comments: false
+  comments: false,
+  plugins
 }
